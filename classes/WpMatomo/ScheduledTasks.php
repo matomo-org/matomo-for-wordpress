88a49b13c393da3ee0b21cb12be025471e8aefae
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-05 16:28:12 +1300

    set hard coded the lastN to 2 to avoid matomo regression matomo-org/matomo#15086

--- classes/WpMatomo/ScheduledTasks.php
+++ classes/WpMatomo/ScheduledTasks.php
@@ -177,6 +177,7 @@ class ScheduledTasks {
 		$archiver                               = new CronArchive();
 		$archiver->concurrentRequestsPerWebsite = 1;
 		$archiver->maxConcurrentArchivers       = 1;
+		$archiver->dateLastForced               = 2;
 
 		if ( $force ) {
 			$archiver->shouldArchiveAllSites        = true;
