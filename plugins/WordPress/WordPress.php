d1831d7a228090666e5e53f7ef0b47aa8654315c
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-20 14:43:28 +1300

    disable flush all when output_compression is enabled

--- plugins/WordPress/WordPress.php
+++ plugins/WordPress/WordPress.php
@@ -232,6 +232,10 @@ class WordPress extends Plugin
     {  
         if ($module === 'Proxy' && in_array($action, array('getNonCoreJs', 'getCoreJs', 'getCss'))) {
             remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+        } else {
+	        if (function_exists('ini_get') && @ini_get('zlib.output_compression') === '1') {
+		        remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+	        }
         }
 	    
         $requestedModule = !empty($module) ? strtolower($module) : '';
