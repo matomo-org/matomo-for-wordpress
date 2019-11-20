e5bef650807992c4ed964d7866ffd0ce3963dabd
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-20 16:45:43 +1300

    Integrate with Matomo Marketplace plugin (#99)

--- classes/WpMatomo/Admin/views/marketplace.php
+++ classes/WpMatomo/Admin/views/marketplace.php
@@ -24,6 +24,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <h1><?php esc_html_e( 'Discover new functionality for your Matomo', 'matomo' ); ?></h1>
     <p><?php esc_html_e( 'Take your Matomo (formerly Piwik) to the next level and drive your conversions & revenue with these premium features. All features are fully hosted on your WordPress and come with 100% data ownership and no limitations.', 'matomo' ); ?></p>
+
+    <?php if ( ! is_plugin_active(MATOMO_MARKETPLACE_PLUGIN_NAME )) { ?>
+    <div class="matomo-hero">
+        <h2>Easily install over 100 free plugins &amp; <span style="white-space: nowrap;">premium features</span> for Matomo with just a click</h2>
+        <a href="https://builds.matomo.org/matomo-marketplace-for-wordpress-latest.zip" rel="noreferrer noopener" target="_blank" class="button matomo-cta-button"><?php esc_html_e('Download Matomo Marketplace for WordPress', 'matomo'); ?></a>
+        <br>
+        <a href="https://matomo.org/faq/wordpress/how-do-i-install-a-matomo-marketplace-plugin-in-matomo-for-wordpress/"><?php esc_html_e('Learn more', 'matomo'); ?></a>
+        <a href="https://plugins.matomo.org/?wp=1" rel="noreferrer noopener" class="matomo-next-link"><?php esc_html_e('Browse Marketplace', 'matomo'); ?></a>
+    </div>
+    <?php } ?>
+
     <?php
     $matomo_feature_sections = array(
         array(
@@ -34,42 +45,15 @@ if ( ! defined( 'ABSPATH' ) ) {
                         'name'        => 'Heatmap & Session Recording',
                         'description' => 'Truly understand your visitors by seeing where they click, hover, type and scroll. Replay their actions in a video and ultimately increase conversions.',
                         'price'       => '99EUR / 119USD',
-                        'url'         => 'https://plugins.matomo.org/HeatmapSessionRecording',
+                        'url'         => 'https://plugins.matomo.org/HeatmapSessionRecording?wp=1',
                         'image'       => plugins_url( 'assets/img/heatmap.jpg', MATOMO_ANALYTICS_FILE ),
-                    ),
-                    array(
-                        'name'        => 'Search Engine Keywords Performance',
-                        'description' => 'All keywords searched by your users on search engines are now visible into your Referrers reports! The ultimate solution to \'Keyword not defined\'.',
-                        'price'       => '69EUR / 79USD',
-                        'url'         => 'https://plugins.matomo.org/SearchEngineKeywordsPerformance',
-                        'image'       => plugins_url( 'assets/img/search_engine_keywords.png', MATOMO_ANALYTICS_FILE ),
                     ),
                     array(
                         'name'        => 'Custom Reports',
                         'description' => 'Pull out the information you need in order to be successful. Develop your custom strategy to meet your individualized goals while saving money & time.',
                         'price'       => '99EUR / 119USD',
-                        'url'         => 'https://plugins.matomo.org/CustomReports',
+                        'url'         => 'https://plugins.matomo.org/CustomReports?wp=1',
                         'image'       => plugins_url( 'assets/img/custom_reports.png', MATOMO_ANALYTICS_FILE ),
-                    ),
-                ),
-        ),
-        array(
-            'title'    => 'Most popular conversion optimisation',
-            'features' =>
-                array(
-                    array(
-                        'name'        => 'Funnels',
-                        'description' => 'Identify and understand where your visitors drop off to increase your conversions, sales and revenue with your existing traffic.',
-                        'price'       => '89EUR / 99USD',
-                        'url'         => 'https://plugins.matomo.org/Funnels',
-                        'image'       => plugins_url( 'assets/img/funnels.png', MATOMO_ANALYTICS_FILE ),
-                    ),
-                    array(
-                        'name'        => 'Multi Attribution',
-                        'description' => 'Get a clear understanding of how much credit each of your marketing channel is actually responsible for to shift your marketing efforts wisely.',
-                        'price'       => '49EUR / 59USD',
-                        'url'         => 'https://plugins.matomo.org/MultiChannelConversionAttribution',
-                        'image'       => plugins_url( 'assets/img/multi_attribution.png', MATOMO_ANALYTICS_FILE ),
                     ),
                 ),
         ),
@@ -81,25 +65,45 @@ if ( ! defined( 'ABSPATH' ) ) {
                         'name'        => 'Form Analytics',
                         'description' => 'Increase conversions on your online forms and lose less visitors by learning everything about your users behavior and their pain points on your forms.',
                         'price'       => '69EUR / 79USD',
-                        'url'         => 'https://plugins.matomo.org/FormAnalytics',
+                        'url'         => 'https://plugins.matomo.org/FormAnalytics?wp=1',
                         'image'       => plugins_url( 'assets/img/form_analytics.jpg', MATOMO_ANALYTICS_FILE ),
                     ),
                     array(
                         'name'        => 'Media Analytics',
                         'description' => 'Grow your business with advanced video & audio analytics. Get powerful insights into how your audience watches your videos and listens to your audio.',
                         'price'       => '69EUR / 79USD',
-                        'url'         => 'https://plugins.matomo.org/MediaAnalytics',
+                        'url'         => 'https://plugins.matomo.org/MediaAnalytics?wp=1',
                         'image'       => plugins_url( 'assets/img/media_analytics.jpg', MATOMO_ANALYTICS_FILE ),
                     ),
                     array(
                         'name'        => 'Users Flow',
                         'description' => 'Users Flow is a visual representation of the most popular paths your users take through your website & app which lets you understand your users needs.',
                         'price'       => '69EUR / 79USD',
-                        'url'         => 'https://plugins.matomo.org/UsersFlow',
+                        'url'         => 'https://plugins.matomo.org/UsersFlow?wp=1',
                         'image'       => plugins_url( 'assets/img/users_flow.png', MATOMO_ANALYTICS_FILE ),
                     ),
                 ),
         ),
+	    array(
+		    'title'    => 'Most popular conversion optimisation',
+		    'features' =>
+			    array(
+				    array(
+					    'name'        => 'Funnels',
+					    'description' => 'Identify and understand where your visitors drop off to increase your conversions, sales and revenue with your existing traffic.',
+					    'price'       => '89EUR / 99USD',
+					    'url'         => 'https://plugins.matomo.org/Funnels?wp=1',
+					    'image'       => plugins_url( 'assets/img/funnels.png', MATOMO_ANALYTICS_FILE ),
+				    ),
+				    array(
+					    'name'        => 'Multi Attribution',
+					    'description' => 'Get a clear understanding of how much credit each of your marketing channel is actually responsible for to shift your marketing efforts wisely.',
+					    'price'       => '49EUR / 59USD',
+					    'url'         => 'https://plugins.matomo.org/MultiChannelConversionAttribution?wp=1',
+					    'image'       => plugins_url( 'assets/img/multi_attribution.png', MATOMO_ANALYTICS_FILE ),
+				    ),
+			    ),
+	    ),
         array(
             'title'    => 'Other Premium Features',
             'features' =>
@@ -108,16 +112,24 @@ if ( ! defined( 'ABSPATH' ) ) {
                         'name'        => 'Cohorts',
                         'description' => 'Track your retention efforts over time and keep your visitors engaged and coming back for more.',
                         'price'       => '49EUR / 59USD',
-                        'url'         => 'https://plugins.matomo.org/Cohorts',
+                        'url'         => 'https://plugins.matomo.org/Cohorts?wp=1',
                         'image'       => plugins_url( 'assets/img/cohorts.png', MATOMO_ANALYTICS_FILE ),
                     ),
+	                array(
+		                'name'        => 'Search Engine Keywords Performance',
+		                'description' => 'All keywords searched by your users on search engines are now visible into your Referrers reports! The ultimate solution to \'Keyword not defined\'.',
+		                'price'       => '69EUR / 79USD',
+		                'url'         => 'https://plugins.matomo.org/SearchEngineKeywordsPerformance?wp=1',
+		                'image'       => plugins_url( 'assets/img/search_engine_keywords.png', MATOMO_ANALYTICS_FILE ),
+                    ),
+                    /*
                     array(
                         'name'        => 'Activity Log',
                         'description' => 'Truly understand your visitors by seeing where they click, hover, type and scroll. Replay their actions in a video and ultimately increase conversions',
                         'price'       => '19EUR / 19USD',
-                        'url'         => 'https://plugins.matomo.org/ActivityLog',
+                        'url'         => 'https://plugins.matomo.org/ActivityLog?wp=1',
                         'image'       => plugins_url( 'assets/img/activity_log.jpg', MATOMO_ANALYTICS_FILE ),
-                    ),
+                    ),*/
                 ),
         ),
     );
