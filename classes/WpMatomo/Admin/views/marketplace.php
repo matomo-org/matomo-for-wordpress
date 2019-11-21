94f0dcf1a7f02a1906f2087dab1af13a671c3da9
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-22 10:20:17 +1300

    tweak wording

--- classes/WpMatomo/Admin/views/marketplace.php
+++ classes/WpMatomo/Admin/views/marketplace.php
@@ -32,13 +32,13 @@ $matomo_extra_url_params = '&' . http_build_query(array(
     <p>
         <?php esc_html_e( 'Take your Matomo (formerly Piwik) to the next level and drive your conversions & revenue with these premium features. All features are fully hosted on your WordPress and come with 100% data ownership and no limitations.', 'matomo' ); ?>
         <?php if ( is_plugin_active(MATOMO_MARKETPLACE_PLUGIN_NAME )) { ?>
-            <a href="https://plugins.matomo.org/?wp=1" target="_blank" rel="noreferrer noopener"><?php esc_html_e('Browse Marketplace', 'matomo'); ?></a>
+            <a href="https://plugins.matomo.org/?wp=1" target="_blank" rel="noreferrer noopener">&#187; <?php esc_html_e('Browse our Marketplace with over 100 free plugins and premium features', 'matomo'); ?></a>
         <?php } ?>
     </p>
 
     <?php if ( ! is_plugin_active(MATOMO_MARKETPLACE_PLUGIN_NAME )) { ?>
     <div class="matomo-hero">
-        <h2>Easily install over 100 free plugins &amp; <span style="white-space: nowrap;">premium features</span> for Matomo with just a click</h2>
+        <h2><?php sprintf( esc_html__('Easily install over 100 free plugins & %1$spremium features%2$s for Matomo with just a click'), '<span style="white-space: nowrap;">', '</span>') ?></h2>
         <a href="https://builds.matomo.org/matomo-marketplace-for-wordpress-latest.zip" rel="noreferrer noopener" class="button matomo-cta-button"><?php esc_html_e('Download Matomo Marketplace for WordPress', 'matomo'); ?></a>
         <br>
         <a target="_blank" href="https://matomo.org/faq/wordpress/how-do-i-install-a-matomo-marketplace-plugin-in-matomo-for-wordpress/"><?php esc_html_e('Learn more', 'matomo'); ?></a>
