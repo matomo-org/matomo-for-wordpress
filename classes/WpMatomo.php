3794fd76c9495139cd8769574f1bf4ab7b7add23
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-28 12:11:16 +1300

    show more information

--- classes/WpMatomo.php
+++ classes/WpMatomo.php
@@ -144,7 +144,7 @@ class WpMatomo {
 						add_action(
 							'admin_notices',
 							function () {
-								echo '<div class="error"><p>' . __( 'It looks like you are maybe using a custom WordPress content directory. The Matomo Analytics plugin likely won\'t fully work.', 'matomo' ) . '</p></div>';
+								echo '<div class="error"><p>' . __( 'It looks like you are maybe using a custom WordPress content directory. The Matomo reporting/admin pages might not work. You may be able to workaround this.', 'matomo' ) . ' <a target="_blank" rel="noreferrer noopener" href="https://matomo.org/faq/wordpress/what-are-the-requirements-for-matomo-for-wordpress/">'. esc_html__('Learn more', 'matomo').'</a>.</p></div>';
 							}
 						);
 					}
