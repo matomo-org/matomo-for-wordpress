b71de8087f66b667c0cb81577dc5647648ef95e3
Thomas Steur <tsteur@users.noreply.github.com>
2019-12-04 08:49:18 +1300

    refs matomo-org/matomo#15232 fix malformed download link

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.3.3
+- Improve test cookie logic
+- Fix malformed download link in real time widget
+
 0.3.2
 - Add method for plugins to detect the context
 
--- app/plugins/Actions/VisitorDetails.php
+++ app/plugins/Actions/VisitorDetails.php
@@ -138,10 +138,14 @@ class VisitorDetails extends VisitorDetailsAbstract
 
         // Reconstruct url from prefix
         if (array_key_exists('url', $action) && array_key_exists('url_prefix', $action)) {
-            $url = PageUrl::reconstructNormalizedUrl($action['url'], $action['url_prefix']);
-            $url = Common::unsanitizeInputValue($url);
+        	if (is_string($action['url'])
+	            && stripos($action['url'], 'http://') !== 0
+	            && stripos($action['url'], 'https://') !== 0) {
+		        $url = PageUrl::reconstructNormalizedUrl($action['url'], $action['url_prefix']);
+		        $url = Common::unsanitizeInputValue($url);
+		        $action['url'] = $url;
+	        }
 
-            $action['url'] = $url;
             unset($action['url_prefix']);
         }
 
