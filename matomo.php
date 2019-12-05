03a84b6f037c116d52ed10cc58b3dd4eb1222f60
Thomas Steur <tsteur@users.noreply.github.com>
2019-12-05 16:39:16 +1300

    update version number since WP seems to already have 1.0.1

--- matomo.php
+++ matomo.php
@@ -4,7 +4,7 @@
  * Description: The #1 Google Analytics alternative that gives you full control over your data and protects the privacy for your users. Free, secure and open.
  * Author: Matomo
  * Author URI: https://matomo.org
- * Version: 1.0.0
+ * Version: 1.0.2
  * Domain Path: /languages
  * WC requires at least: 2.4.0
  * WC tested up to: 3.8
