e6dae7bbc42938366a335e7abbd7f05088ae349f
Thomas Steur <tsteur@users.noreply.github.com>
2020-01-02 13:51:42 +1300

    various tweaks

--- app/core/Db/Adapter/Pdo/Mysql.php
+++ app/core/Db/Adapter/Pdo/Mysql.php
@@ -35,7 +35,7 @@ class Mysql extends Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
         if (defined('PDO::MYSQL_ATTR_LOCAL_INFILE')) {
             $config['driver_options'][PDO::MYSQL_ATTR_LOCAL_INFILE] = true;
         }
-        if ($config['enable_ssl']) {
+        if (!empty($config['enable_ssl'])) {
             if (!empty($config['ssl_key'])) {
                 $config['driver_options'][PDO::MYSQL_ATTR_SSL_KEY] = $config['ssl_key'];
             }
