17d367c268ee0fd724458a6a8979c47e65500570
Thomas Steur <tsteur@users.noreply.github.com>
2020-01-03 14:04:32 +1300

    fix possible notice in geoip updater

--- app/plugins/GeoIp2/LocationProvider/GeoIp2.php
+++ app/plugins/GeoIp2/LocationProvider/GeoIp2.php
@@ -153,6 +153,7 @@ abstract class GeoIp2 extends LocationProvider
 		static $result = null;
 		if (is_null($result)) {
 			$expected = array(self::COUNTRY_CODE_KEY => 'FR',
+			                  self::REGION_CODE_KEY => 'BFC',
 			                  self::CITY_NAME_KEY    => 'Besançon');
 			$result = array(self::TEST_IP, $expected);
 		}
