a491088f5e480569855f88944ae3b06e9f788a0f
Thomas Steur <tsteur@users.noreply.github.com>
2020-01-07 12:00:39 +1300

    few fixes

--- classes/WpMatomo/Email.php
+++ classes/WpMatomo/Email.php
@@ -20,9 +20,10 @@ class Email extends \Zend_Mail_Transport_Abstract {
 	 * @var \WP_Error|null
 	 */
 	private $wpMailError;
+
 	private $wpContentType = null;
 
-	public function onError ($error) {
+	public function onError($error) {
 		$this->wpMailError = $error;
 	}
 
@@ -35,18 +36,31 @@ class Email extends \Zend_Mail_Transport_Abstract {
 	}
 
 	public function send(Zend_Mail $mail) {
+		$this->wpContentType = null;
 		$this->wpMailError = null;
+
+		if ($mail->hasAttachments) {
+			// we prefer sending the multipart message through _sendMail()
+			// see #183 the problem is that we can't attach the raw attachment to WP_Mail which is why we need to rely
+			// on the encoded message. This seems to work with built-in wp_mail but other custom implementations may have
+			// issues which is why we are using plain text/body by default below when there are no attachments
+			parent::send($mail);
+			return;
+		}
+
+		// we prefer sending the plain text or html message
 		$this->_mail = $mail;
 
-		add_action( 'wp_mail_failed' , array($this, 'onError') );
-		add_filter( 'wp_mail_content_type' , array($this, 'setContentType'));
-
-		if ($mail->getBodyHtml(true)) {
-			$content = $mail->getBodyHtml(true);
+		if ($mail->getBodyHtml() && $mail->getBodyHtml()->getRawContent()) {
+			$content = $mail->getBodyHtml()->getRawContent();
 			$this->wpContentType = 'text/html';
-		} else {
-			$content = $mail->getBodyText(true);
+		} elseif ($mail->getBodyText()) {
+			$content = $mail->getBodyText()->getRawContent();
 			$this->wpContentType = 'text/plain';
+		} else {
+			// seems no content... lets use _sendMail
+			parent::send($mail);
+			return;
 		}
 
 		$headers = $mail->getHeaders();
@@ -55,34 +69,58 @@ class Email extends \Zend_Mail_Transport_Abstract {
 		}
 		$this->_prepareHeaders($headers);
 
-		$attachments = array();
-		if ($mail->hasAttachments) {
-			for ($i = 0; $i < $mail->getPartCount(); $i++) {
-				$attachments[] = $mail->getPartContent($i);
+		$this->sendMailThroughWordPress($mail->getRecipients(), $mail->getSubject(), $content, $this->header);
+	}
+
+	public function _sendMail() {
+
+		if (!empty($this->_headers['Content-Type'][0])) {
+			$content = trim( $this->_headers['Content-Type'][0] );
+			if ( strpos( $content, ';' ) !== false ) {
+				list( $type, $charset_content ) = explode( ';', $content );
+				$type = trim( $type );
+				if (!empty($type)) {
+					$this->wpContentType = $type;
+				}
+
+			} elseif ( !empty($content) ) {
+				$this->wpContentType = $content;
 			}
 		}
 
-		$success = wp_mail( $mail->getRecipients(), $mail->getSubject(), $content, $this->header );
+		// used when we are dealing with attachment... we send the raw multipart body which includes attachments
+		$this->sendMailThroughWordPress($this->recipients, $this->_mail->getSubject(), $this->body, $this->header);
+	}
 
-		$this->wpContentType = null;
+	private function sendMailThroughWordPress($recipients, $subject, $content, $header) {
+
+		$this->wpMailError = null;
+
+		add_action( 'wp_mail_failed' , array($this, 'onError') );
+		add_filter( 'wp_mail_content_type' , array($this, 'setContentType'));
+
+		$success = wp_mail( $recipients, $subject, $content, $header );
 
 		remove_action( 'wp_mail_failed', array($this, 'onError') );
 		remove_filter('wp_mail_content_type', array($this, 'setContentType'));
 
 		if (!$success) {
-			$message = 'Error unknown';
+			$message = 'Error unknown.';
 			if (!empty($this->wpMailError) && is_object($this->wpMailError) && $this->wpMailError instanceof \WP_Error) {
 				$message = $this->wpMailError->get_error_message();
+			}
+			if ($this->_mail && $this->_mail->hasAttachments) {
+				$message .= ' (has attachments)';
+			}
+			if ($this->wpContentType) {
+				$message .= ' (type '. $this->wpContentType .')';
 			}
 			$logger = new Logger();
 			$logger->log_exception('matomo_mail', new \Exception($message));
 		}
 
+		$this->wpContentType = null;
 		$this->wpMailError = null;
-	}
-
-	public function _sendMail() {
-		// TODO: Implement _sendMail() method.
 	}
 
 }
