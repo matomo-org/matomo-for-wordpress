85e3756f4507a4232e158c681deb1eb8c3b85ed0
Thomas Steur <tsteur@users.noreply.github.com>
2020-02-11 08:48:45 +1300

    Update remove_not_needed_assets.sh

--- scripts/remove_not_needed_assets.sh
+++ scripts/remove_not_needed_assets.sh
@@ -6,13 +6,17 @@ MATOMO_ROOT="$SCRIPTPATH/../app/";
 # we need to remove jquery as it is shipped with wordpress and we use their jquery
 rm -rf $MATOMO_ROOT/libs/bower_components/jquery
 rm -rf $MATOMO_ROOT/libs/bower_components/jquery-ui
+rm -rf $MATOMO_ROOT/config/environment
+rm -rf $MATOMO_ROOT/app/vendor/twig/twig/ext
 
 # lets remove some extra libs that aren't needed
 find $MATOMO_ROOT/libs/bower_components -name '*.js' -exec rm -rf {} +
 find $MATOMO_ROOT/libs/bower_components -name '*.map' -exec rm -rf {} +
 find $MATOMO_ROOT/libs/jqplot -name '*.js' -exec rm -rf {} +
 find $MATOMO_ROOT/plugins/*/angularjs -name '*.js' -exec rm -rf {} +
-find $MATOMO_ROOT/plugins/ -d  -name 'lang' -empty -delete
 find $MATOMO_ROOT/plugins/*/javascripts -name '*.js' ! -name 'Piwik_Overlay.js' ! -name 'optOut.js' ! -name 'previewmode.js' ! -name 'previewmodedetection.js' ! -name 'tagmanager.js' ! -name 'tagmanager.min.js' -exec rm -rf {} +
+
+# delete some empty directories like config, angularjs, javascripts, lang, ...
+find $MATOMO_ROOT/plugins/ -d  -empty -delete
 
 echo -e "Done!... "
