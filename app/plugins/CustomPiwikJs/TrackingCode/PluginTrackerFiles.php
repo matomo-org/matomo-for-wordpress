c8698e3fc2c295932d24ca24a5f435c60ee092f4
Thomas Steur <tsteur@users.noreply.github.com>
2020-02-14 12:23:26 +1300

    refs matomo-org/matomo#15568 fix tracker files not found in matomo plugins

--- app/plugins/CustomPiwikJs/TrackingCode/PluginTrackerFiles.php
+++ app/plugins/CustomPiwikJs/TrackingCode/PluginTrackerFiles.php
@@ -13,98 +13,84 @@ use Piwik\Plugins\CustomPiwikJs\File;
 
 class PluginTrackerFiles
 {
-    const TRACKER_FILE = 'tracker.js';
-    const MIN_TRACKER_FILE = 'tracker.min.js';
+	const TRACKER_FILE = 'tracker.js';
+	const MIN_TRACKER_FILE = 'tracker.min.js';
 
-    /**
-     * @var string
-     */
-    protected $dir;
+	/**
+	 * @var Plugin\Manager
+	 */
+	private $pluginManager;
 
-    /**
-     * @var Plugin\Manager
-     */
-    private $pluginManager;
+	/**
+	 * @var bool
+	 */
+	protected $ignoreMinified = false;
 
-    /**
-     * @var bool
-     */
-    protected $ignoreMinified = false;
+	public function __construct()
+	{
+		$this->pluginManager = Plugin\Manager::getInstance();
+	}
 
-    public function __construct()
-    {
-        $this->dir = PIWIK_DOCUMENT_ROOT . '/plugins/';
-        $this->pluginManager = Plugin\Manager::getInstance();
-    }
+	public function ignoreMinified()
+	{
+		$this->ignoreMinified = true;
+	}
 
-    public function ignoreMinified()
-    {
-        $this->ignoreMinified = true;
-    }
+	protected function getDirectoriesToLook()
+	{
+		$dirs = array();
+		$manager = Plugin\Manager::getInstance();
+		foreach ($manager->getPluginsLoadedAndActivated() as $pluginName => $plugin) {
+			if ($plugin->isTrackerPlugin()) {
+				$dirs[$pluginName] = rtrim(Plugin\Manager::getPluginDirectory($pluginName), '/') . '/';
+			}
+		}
+		return $dirs;
+	}
 
-    /**
-     * @return File[]
-     */
-    public function find()
-    {
-        $jsFiles = array();
+	/**
+	 * @return File[]
+	 */
+	public function find()
+	{
+		$jsFiles = array();
 
-        if (!$this->ignoreMinified) {
-            $trackerFiles = \_glob($this->dir . '*/' . self::MIN_TRACKER_FILE);
+		foreach ($this->getDirectoriesToLook() as $pluginName => $pluginDir) {
+			if (!$this->ignoreMinified && file_exists($pluginDir . self::MIN_TRACKER_FILE)) {
+				$jsFiles[$pluginName] = new File($pluginDir . self::MIN_TRACKER_FILE);
+			} elseif (file_exists($pluginDir . self::TRACKER_FILE)) {
+				$jsFiles[$pluginName] = new File($pluginDir . self::TRACKER_FILE);
+			}
+		}
 
-            foreach ($trackerFiles as $trackerFile) {
-                $plugin = $this->getPluginNameFromFile($trackerFile);
-                if ($this->isPluginActivated($plugin)) {
-                    $jsFiles[$plugin] = new File($trackerFile);
-                }
-            }
-        }
+		foreach ($jsFiles as $plugin => $file) {
+			if (!$this->shouldIncludeFile($plugin)) {
+				unset($jsFiles[$plugin]);
+			}
+		}
 
-        $trackerFiles = \_glob($this->dir . '*/' . self::TRACKER_FILE);
+		return $jsFiles;
+	}
 
-        foreach ($trackerFiles as $trackerFile) {
-            $plugin = $this->getPluginNameFromFile($trackerFile);
-            if (!isset($jsFiles[$plugin])) {
-                if ($this->isPluginActivated($plugin)) {
-                    $jsFiles[$plugin] = new File($trackerFile);
-                }
-            }
-        }
+	protected function shouldIncludeFile($pluginName)
+	{
+		$shouldAddFile = true;
 
-        foreach ($jsFiles as $plugin => $file) {
-            if (!$this->shouldIncludeFile($plugin)) {
-                unset($jsFiles[$plugin]);
-            }
-        }
+		/**
+		 * Detect if a custom tracker file should be added to the piwik.js tracker or not.
+		 *
+		 * This is useful for example if a plugin only wants to add its tracker file when the plugin is configured.
+		 *
+		 * @param bool &$shouldAddFile Decides whether the tracker file belonging to the given plugin should be added or not.
+		 * @param string $pluginName The name of the plugin this file belongs to
+		 */
+		Piwik::postEvent('CustomPiwikJs.shouldAddTrackerFile', array(&$shouldAddFile, $pluginName));
 
-        return $jsFiles;
-    }
+		return $shouldAddFile;
+	}
 
-    protected function shouldIncludeFile($pluginName)
-    {
-        $shouldAddFile = true;
-
-        /**
-         * Detect if a custom tracker file should be added to the piwik.js tracker or not.
-         *
-         * This is useful for example if a plugin only wants to add its tracker file when the plugin is configured.
-         *
-         * @param bool &$shouldAddFile Decides whether the tracker file belonging to the given plugin should be added or not.
-         * @param string $pluginName The name of the plugin this file belongs to
-         */
-        Piwik::postEvent('CustomPiwikJs.shouldAddTrackerFile', array(&$shouldAddFile, $pluginName));
-
-        return $shouldAddFile;
-    }
-
-    protected function isPluginActivated($pluginName)
-    {
-        return $this->pluginManager->isPluginActivated($pluginName);
-    }
-
-    protected function getPluginNameFromFile($file)
-    {
-        $file = str_replace(array($this->dir, self::TRACKER_FILE, self::MIN_TRACKER_FILE), '', $file);
-        return trim($file, '/');
-    }
+	protected function isPluginActivated($pluginName)
+	{
+		return $this->pluginManager->isPluginActivated($pluginName);
+	}
 }
