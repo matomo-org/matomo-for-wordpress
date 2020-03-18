fa5e7872bc3313ba4ef2244e81d23328344f529a
Thomas Steur <tsteur@users.noreply.github.com>
2020-03-18 14:42:50 +1300

    Mark as tested with Woocommerce 4.0

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 1.0.4
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 3.9.2
+ * WC tested up to: 4.0.0
  *
  * Matomo - free/libre analytics platform
  *
