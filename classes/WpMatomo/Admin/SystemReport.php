4bd2eef9901101bdd892be2e9a5d2f7561cf6041
Thomas Steur <tsteur@users.noreply.github.com>
2020-03-24 17:01:28 +1300

    mark it as a warning

--- classes/WpMatomo/Admin/SystemReport.php
+++ classes/WpMatomo/Admin/SystemReport.php
@@ -700,6 +700,7 @@ class SystemReport {
 			$rows[] = array(
 				'name'  => 'Compatible content directory',
 				'value' => $compatible_content_dir,
+				'is_warning' => true,
 				'comment' =>  __( 'It looks like you are maybe using a custom WordPress content directory. The Matomo reporting/admin pages might not work. You may be able to workaround this.', 'matomo' ) . ' ' . __( 'Learn more', 'matomo' ) . ': https://matomo.org/faq/wordpress/what-are-the-requirements-for-matomo-for-wordpress/'
 			);
 		}
