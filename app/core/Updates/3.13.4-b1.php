cf61d1de48f52293a3a2e96c4f59de1bd0aca8f9
Thomas Steur <tsteur@users.noreply.github.com>
2020-04-23 10:22:34 +1200

    make sure to execute previous 3.13.4 update only for IIS servers

--- app/core/Updates/3.13.4-b1.php
+++ app/core/Updates/3.13.4-b1.php
@@ -12,12 +12,15 @@ namespace Piwik\Updates;
 use Piwik\Plugins\Installation\ServerFilesGenerator;
 use Piwik\Updater;
 use Piwik\Updates as PiwikUpdates;
+use Piwik\SettingsServer;
 
 class Updates_3_13_4_b1 extends PiwikUpdates
 {
     public function doUpdate(Updater $updater)
     {
-        // Fix issue with HeatmapSessionRecording on IIS (https://github.com/matomo-org/matomo/issues/15651)
-        ServerFilesGenerator::createFilesForSecurity();
+	    if (SettingsServer::isIIS()) {
+		    // Fix issue with HeatmapSessionRecording on IIS (https://github.com/matomo-org/matomo/issues/15651)
+		    ServerFilesGenerator::createFilesForSecurity();
+	    }
     }
 }
