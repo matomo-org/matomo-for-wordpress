a77051ba76308b6f466807ff1f1aa8b58cc2e031
Thomas Steur <tsteur@users.noreply.github.com>
2020-06-12 13:34:43 +1200

    fix tests

--- classes/WpMatomo/TrackingCode/TrackingCodeGenerator.php
+++ classes/WpMatomo/TrackingCode/TrackingCodeGenerator.php
@@ -156,7 +156,7 @@ class TrackingCodeGenerator {
 ' . self::MTM_INIT . '
 _mtm.push({\'mtm.startTime\': (new Date().getTime()), \'event\': \'mtm.Start\'});
 var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
-g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src="' . $container_url . '"; s.parentNode.insertBefore(g,s);
+g.type=\'text/javascript\'; g.async=true; g.src="' . $container_url . '"; s.parentNode.insertBefore(g,s);
 </script>';
 				}
 			}
