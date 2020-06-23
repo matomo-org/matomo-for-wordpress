5a0fc2e6cef7c99d8fb52d224e7f66206d67d8a0
Thomas Steur <tsteur@users.noreply.github.com>
2020-06-24 11:31:25 +1200

    Update branch name -> use live

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 VERSION="$1"
 
-hub release create --message $VERSION --message "If you download this release, make sure the directory name within the 'wordpress/wp-content' directory is 'matomo' and not for example 'wp-matomo'. [View changes](https://github.com/matomo-org/wp-matomo/blob/develop/CHANGELOG.md)"  -t master  $VERSION
+hub release create --message $VERSION --message "If you download this release, make sure the directory name within the 'wordpress/wp-content' directory is 'matomo' and not for example 'wp-matomo'. [View changes](https://github.com/matomo-org/wp-matomo/blob/develop/CHANGELOG.md)"  -t live  $VERSION
