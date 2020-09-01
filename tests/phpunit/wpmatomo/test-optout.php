3266a1bb58a995922706c9dd61870362c0f1c897
Thomas Steur <tsteur@users.noreply.github.com>
2020-09-01 15:46:59 +1200

    we now need to bootstrap matomo

--- tests/phpunit/wpmatomo/test-optout.php
+++ tests/phpunit/wpmatomo/test-optout.php
@@ -5,7 +5,7 @@ use WpMatomo\Admin\PrivacySettings;
 /**
  * @package matomo
  */
-class OptOutTest extends MatomoUnit_TestCase {
+class OptOutTest extends MatomoAnalytics_TestCase {
 
 	public function setUp() {
 		parent::setUp();
@@ -19,11 +19,6 @@ class OptOutTest extends MatomoUnit_TestCase {
 	public function test_matomo_opt_out_all_options() {
 		$result = do_shortcode( PrivacySettings::EXAMPLE_FULL );
 		$this->assertSame( '<iframe style="border: 0; width:500px;height:100px;" src="http://example.org/wp-content/plugins/matomo/app/index.php?module=CoreAdminHome&action=optOut&language=de&backgroundColor=red&fontColor=fff&fontSize=34&fontFamily=Arial"></iframe>', $result );
-	}
-
-	public function test_matomo_opt_out_size_percent_px_values() {
-		$result = do_shortcode( '[matomo_opt_out width=60px height=100%]' );
-		$this->assertContains( 'width:60px;height:100%', $result );
 	}
 
 	public function test_optOutJs_exists() {
