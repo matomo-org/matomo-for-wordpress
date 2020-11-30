bfa0d0a4966bd6b76f024faf48906cdcda42fd2f
Thomas Steur <tsteur@users.noreply.github.com>
2020-11-30 14:12:08 +1300

    apply patches from core

--- app/plugins/Referrers/Columns/ReferrerUrl.php
+++ app/plugins/Referrers/Columns/ReferrerUrl.php
@@ -15,37 +15,50 @@ use Piwik\Tracker\Action;
 
 class ReferrerUrl extends Base
 {
-    protected $columnName = 'referer_url';
-    protected $columnType = 'VARCHAR(4000) NULL';
-    protected $type = self::TYPE_TEXT;
-    protected $segmentName = 'referrerUrl';
-    protected $nameSingular = 'Live_Referrer_URL';
-    protected $namePlural = 'Referrers_ReferrerURLs';
-    protected $category = 'Referrers_Referrers';
-    protected $acceptValues = 'http%3A%2F%2Fwww.example.org%2Freferer-page.htm';
+	const MAX_LEN = 1500;
+	protected $columnName = 'referer_url';
+	protected $type = self::TYPE_TEXT;
+	protected $segmentName = 'referrerUrl';
+	protected $nameSingular = 'Live_Referrer_URL';
+	protected $namePlural = 'Referrers_ReferrerURLs';
+	protected $category = 'Referrers_Referrers';
+	protected $acceptValues = 'http%3A%2F%2Fwww.example.org%2Freferer-page.htm';
 
-    /**
-     * @param Request $request
-     * @param Visitor $visitor
-     * @param Action|null $action
-     * @return mixed
-     */
-    public function onNewVisit(Request $request, Visitor $visitor, $action)
-    {
-        $information = $this->getReferrerInformationFromRequest($request, $visitor);
+	public function __construct()
+	{
+		$this->columnType = 'VARCHAR('.self::MAX_LEN.') NULL';
+	}
 
-        return $information['referer_url'];
-    }
+	/**
+	 * @param Request $request
+	 * @param Visitor $visitor
+	 * @param Action|null $action
+	 * @return mixed
+	 */
+	public function onNewVisit(Request $request, Visitor $visitor, $action)
+	{
+		$information = $this->getReferrerInformationFromRequest($request, $visitor);
 
-    public function onExistingVisit(Request $request, Visitor $visitor, $action)
-    {
-        $information = $this->getReferrerInformationFromRequest($request, $visitor);
-        if ($this->isCurrentReferrerDirectEntry($visitor)
-            && $information['referer_type'] != Common::REFERRER_TYPE_DIRECT_ENTRY
-        ) {
-            return $information['referer_url'];
-        }
+		return $this->trimUrl($information['referer_url']);
+	}
 
-        return false;
-    }
+	private function trimUrl($url)
+	{
+		if (!empty($url) && is_string($url) && Common::mb_strlen($url) > self::MAX_LEN) {
+			return Common::mb_substr($url, 0, self::MAX_LEN);
+		}
+		return $url;
+	}
+
+	public function onExistingVisit(Request $request, Visitor $visitor, $action)
+	{
+		$information = $this->getReferrerInformationFromRequest($request, $visitor);
+		if ($this->isCurrentReferrerDirectEntry($visitor)
+		    && $information['referer_type'] != Common::REFERRER_TYPE_DIRECT_ENTRY
+		) {
+			return $this->trimUrl($information['referer_url']);
+		}
+
+		return false;
+	}
 }
