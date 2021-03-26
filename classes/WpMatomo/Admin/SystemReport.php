4b0aed802cf96e6c3aaf0348ad2ff9d74c99dac6
Thomas Steur <tsteur@users.noreply.github.com>
2021-03-26 13:13:19 +1300

    no longer mark cookiebot as incompatible

--- classes/WpMatomo/Admin/SystemReport.php
+++ classes/WpMatomo/Admin/SystemReport.php
@@ -56,7 +56,6 @@ class SystemReport {
 		'data-tables-generator-by-supsystic', // uses an old version of twig causing some styles to go funny in the reporting and admin
 		'tweet-old-post-pro', // uses a newer version of monolog
 		'secupress', // see #369 depending on setting might have issues
-		'cookiebot', // see https://wordpress.org/support/topic/critical-error-after-upgrade/ conflict re php-di version
 		'wp-rss-aggregator', // see https://wordpress.org/support/topic/critical-error-after-upgrade/ conflict re php-di version
 		'wp-defender', // see https://wordpress.org/support/topic/critical-error-after-upgrade/ conflict re php-di version
 		'age-verification-for-woocommerce', // see https://github.com/matomo-org/wp-matomo/issues/428
