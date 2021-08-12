be4b7e3b2871d603ecbf4f4115614cdc25d82e0e
Matthieu <matthieu.mary@innocraft.com>
2021-08-12 15:36:21 +1200

    use the minified version of chart.js

--- classes/WpMatomo/Admin/Chart.php
+++ classes/WpMatomo/Admin/Chart.php
@@ -13,7 +13,7 @@ class Chart {
 	}
 
 	public function load_chartjs() {
-		wp_enqueue_script('chart.js', plugins_url( 'node_modules/chart.js/dist/chart.js', MATOMO_ANALYTICS_FILE ) );
+		wp_enqueue_script('chart.js', plugins_url( 'node_modules/chart.js/dist/chart.min.js', MATOMO_ANALYTICS_FILE ) );
 		wp_enqueue_script('matomo_chart.js', plugins_url( 'assets/chart.js', MATOMO_ANALYTICS_FILE ) );
 	}
 }
