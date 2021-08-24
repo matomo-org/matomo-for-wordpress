d4e98036cd6ff4d3b471b351253967da8844106c
Thomas Steur <tsteur@users.noreply.github.com>
2021-08-25 10:45:58 +1200

    Fix: assets getting force downloaded
    refs https://github.com/10up/action-wordpress-plugin-asset-update/pull/21

--- scripts/deploy_marketplace_only.sh
+++ scripts/deploy_marketplace_only.sh
@@ -77,6 +77,11 @@ rsync -rc "$TMP_DIR/" trunk/ --delete --delete-excluded
 # Copy dotorg assets to /assets
 rsync -rc "$GITHUB_WORKSPACE/$ASSETS_DIR/" assets/ --delete --delete-excluded
 
+# Fix screenshots getting force downloaded when clicking them
+# https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/
+svn propset svn:mime-type image/png assets/*.png || true
+svn propset svn:mime-type image/jpeg assets/*.jpg || true
+
 echo "➤ Preparing files..."
 
 svn status
