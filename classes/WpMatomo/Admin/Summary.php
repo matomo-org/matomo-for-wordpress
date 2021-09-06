6469a122f3fdf46d939e790ecd350b1c48c5feba
mattmary <86216084+mattmary@users.noreply.github.com>
2021-09-06 13:15:16 +1200

    Restore the default date management (#504)

--- classes/WpMatomo/Admin/Summary.php
+++ classes/WpMatomo/Admin/Summary.php
@@ -14,6 +14,7 @@ use WpMatomo\Report\Dates;
 use WpMatomo\Report\Metadata;
 use WpMatomo\Report\Renderer;
 use WpMatomo\Settings;
+use Piwik\Plugins\UsersManager\UserPreferences;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // if accessed directly
@@ -44,11 +45,9 @@ class Summary {
 			 && current_user_can( Capabilities::KEY_VIEW ) ) {
 			$unique_id = sanitize_text_field( wp_unslash( $_GET['report_uniqueid'] ) );
 			$date      = sanitize_text_field( wp_unslash( $_GET['report_date'] ) );
-
 			$dashobard = new Dashboard();
 			if ( $dashobard->is_valid_widget( $unique_id, $date ) ) {
 				$dashobard->toggle_widget( $unique_id, $date );
-
 				return true;
 			}
 		}
@@ -58,6 +57,7 @@ class Summary {
 
 	public function show() {
 		do_action( 'matomo_load_chartjs' );
+    
 		$matomo_pinned = $this->pin_if_submitted();
 
 		$settings = $this->settings;
@@ -68,7 +68,40 @@ class Summary {
 		$report_dates_obj = new Dates();
 		$report_dates     = $report_dates_obj->get_supported_dates();
 
-		$report_date = Dates::YESTERDAY;
+		$user_preference = new UserPreferences();
+		$default_date    = $user_preference->getDefaultDate();
+		$report_period   = $user_preference->getDefaultPeriod();
+		switch ( $report_period ) {
+			case 'day':
+				$report_date = $default_date;
+				break;
+			case 'year':
+			case 'month':
+			case 'week':
+				switch ( $default_date ) {
+					case 'yesterday':
+						$report_date = 'last' . $report_period;
+						break;
+					case 'today':
+						$report_date = 'this' . $report_period;
+						break;
+				}
+				break;
+			case 'range':
+				switch ( $default_date ) {
+					case 'previous30':
+						$report_date = 'lastmonth';
+						break;
+					case 'previous7':
+						$report_date = 'lastweek';
+						break;
+					case 'last30':
+						$report_date = 'thismonth';
+						break;
+					case 'last7':
+						$report_date = 'thisweek';
+				}
+		}
 		if ( isset( $_GET['report_date'] ) && isset( $report_dates[ $_GET['report_date'] ] ) ) {
 			$report_date = sanitize_text_field( wp_unslash( $_GET['report_date'] ) );
 		}
