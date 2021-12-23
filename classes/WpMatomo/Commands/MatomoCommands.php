1a2595b47de11ca1f0e19d4e218074ad8712c572
Matthieu <matthieu.mary@innocraft.com>
2021-12-24 09:14:28 +1300

    checkstyle and add a test if wpstatistics plugin is activated

--- classes/WpMatomo/Commands/MatomoCommands.php
+++ classes/WpMatomo/Commands/MatomoCommands.php
@@ -66,48 +66,38 @@ class MatomoCommands extends WP_CLI_Command {
 	 *
 	 * [--site=<siteId>]
 	 * : the site id to import
-	 * [--loggin=<logging>]
-	 * : Your log in
-	 * [--password=<password>]
-	 * : Your password
 	 * ## EXAMPLES
 	 *
-	 *     wp matomo update --site 1
+	 *     wp matomo importWpStatistics --site 1
 	 *
 	 * @when after_wp_load
 	 */
 	public function importWpStatistics( $args, $assoc_args ) {
 		$logger = new WpCliLogger();
+		if ( !is_plugin_active( 'wp-statistics/wp-statistics.php' ) ) {
+			$logger->error( 'Plugin wpstatistics must be installed and activated' );
+			return;
+		}
 		$logger->info( 'Starting wp-statistics import'  );
 		try {
 			Bootstrap::do_bootstrap();
 			Access::getInstance()->setSuperUserAccess(true);
-			$loggin = ! empty( $assoc_args['loggin'] ) ? $assoc_args['loggin'] : null;
-			$password = ! empty( $assoc_args['password'] ) ? $assoc_args['password'] : null;
-			$creds = array();
-			$creds['user_login'] = $loggin;
-			$creds['user_password'] =  $password;
-			$creds['remember'] = true;
-			$user = wp_signon( $creds, false );
 			$importer = new Importer($logger);
 			if ( function_exists( 'is_multisite' ) && is_multisite() && function_exists( 'get_sites' ) ) {
 				$id_site = ! empty( $assoc_args['site'] ) ? $assoc_args['site'] : null;
-				$logger->info( 'Function exists'  );
 				foreach ( get_sites() as $site ) {
 					/** @var WP_Site $site */
-					if ( is_null( $id_site ) || ( $site->blog_id === $id_site ) ) {
-						$logger->info( 'Switch to blog'  );
+					if ( is_null( $id_site ) || ( $site->site_id === $id_site ) ) {
 						switch_to_blog( $site->blog_id );
 						// this way we make sure all blogs get updated eventually
-						$logger->info( 'Blog ID' . $site->blog_id );
+						$logger->info( 'Blog ID ' . $site->blog_id );
 						$importer->import( $site->blog_id );
 						restore_current_blog();
 					}
 				}
 			} else {
-				$id_site = ! empty( $assoc_args['site'] ) ? $assoc_args['site'] : 1;
-				switch_to_blog( $id_site );
-			//	set_error_handler([$this, 'myErrorHandler']);
+				$id_site = 1;
+				switch_to_blog( 1 );
 				$importer->import( $id_site );
 			}
 
@@ -115,43 +105,6 @@ class MatomoCommands extends WP_CLI_Command {
 		} catch (\Exception $e) {
 			$logger->error($e->getMessage());
 		}
-
-	}
-
-	function myErrorHandler($errno, $errstr, $errfile, $errline)
-	{
-		if (!(error_reporting() & $errno)) {
-			// This error code is not included in error_reporting, so let it fall
-			// through to the standard PHP error handler
-			return false;
-		}
-
-		// $errstr may need to be escaped:
-		$errstr = htmlspecialchars($errstr);
-
-		switch ($errno) {
-			case E_USER_ERROR:
-				echo "<b>My ERROR</b> [$errno] $errstr<br />\n";
-				echo "  Fatal error on line $errline in file $errfile";
-				echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />\n";
-				echo "Aborting...<br />\n";
-				exit(1);
-
-			case E_USER_WARNING:
-				echo "<b>My WARNING</b> [$errno] $errstr<br />\n";
-				break;
-
-			case E_USER_NOTICE:
-				echo "<b>My NOTICE</b> [$errno] $errstr<br />\n";
-				break;
-
-			default:
-				echo "Unknown error type: [$errno] $errstr<br />\n";
-				break;
-		}
-
-		/* Don't execute PHP internal error handler */
-		return true;
 	}
 	/**
 	 * Updates Matomo.
