fd52995aceff8a6745990b24f91dc3261e40de3a
Matthieu <matthieu.mary@innocraft.com>
2022-01-07 08:06:24 +1300

    try to cluster pages urls

--- classes/WpMatomo/WpStatistics/DataConverters/PagesUrlConverter.php
+++ classes/WpMatomo/WpStatistics/DataConverters/PagesUrlConverter.php
@@ -2,9 +2,34 @@
 
 namespace WpMatomo\WpStatistics\DataConverters;
 
+use Piwik\DataTable;
+use Piwik\Plugins\Actions\ArchivingHelper;
+use Piwik\Tracker\Action;
+
 class PagesUrlConverter extends NumberConverter implements DataConverterInterface {
 
 	public static function convert( array $wpStatisticsData ) {
-		return self::aggregateByKey( $wpStatisticsData, 'str_url' );
+		$rows = self::aggregateByKey( $wpStatisticsData, 'str_url' );
+		$mainUrlWithoutSlash = site_url();
+		$mainUrlWithoutSlash = rtrim($mainUrlWithoutSlash, '/');
+		$dataTables = [
+			Action::TYPE_PAGE_URL => new DataTable()
+		];
+		foreach($rows as $row) {
+			$wholeUrl = $mainUrlWithoutSlash . $row->getColumn('label');
+			
+			$actionRow = ArchivingHelper::getActionRow('dummyhost.com' . $row->getColumn('label'), Action::TYPE_PAGE_URL, '', $dataTables);
+
+			$row->deleteColumn('label');
+
+			$actionRow->sumRow($row, $copyMetadata = false);
+
+			if ($actionRow->getColumn('label') != DataTable::LABEL_SUMMARY_ROW) {
+				$actionRow->setMetadata('url', $wholeUrl);
+			}
+
+			//$this->pageUrlsByPagePath[$wholeUrl] = $actionRow;
+		}
+		return $dataTables[Action::TYPE_PAGE_URL];
 	}
 }
