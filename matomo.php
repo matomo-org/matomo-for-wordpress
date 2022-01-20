386f6ae977da1e62e7689ce2c4112f3c0577501c
mattmary <86216084+mattmary@users.noreply.github.com>
2022-01-21 07:42:06 +1300

    Update matomo.php
    update wc supported version

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 4.6.0
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 6.0.0
+ * WC tested up to: 6.1.1
  *
  * Matomo - free/libre analytics platform
  *
