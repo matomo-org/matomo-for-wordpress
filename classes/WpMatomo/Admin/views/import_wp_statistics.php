7057a49f893ffc0d6a5f98db3460d1b8e8c92f9b
Matthieu <matthieu.mary@innocraft.com>
2022-02-24 15:55:00 +1300

    phpcs

--- classes/WpMatomo/Admin/views/import_wp_statistics.php
+++ classes/WpMatomo/Admin/views/import_wp_statistics.php
@@ -32,14 +32,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php echo sprintf( esc_html__( 'The WP-CLI is the official WordPress client command line tool. Follow these installation instructions: %s.', 'matomo' ), '<a href="https://wp-cli.org/#installing" rel="noreferrer noopener" target="_blank">here</a>' ); ?>
 	<h2>2. <?php esc_html_e( 'Run the import', 'matomo' ); ?></h2>
 
-	<?php echo sprintf( esc_html__( 'Run the command %1$s or depending on your set-up you may need to run it like this: %2$s.', 'matomo' ), '<code>php wp-cli.phar matomo importWpStatistics</code>','<code>wp matomo importWpStatistics</code>' ); ?>
+	<?php echo sprintf( esc_html__( 'Run the command %1$s or depending on your set-up you may need to run it like this: %2$s.', 'matomo' ), '<code>php wp-cli.phar matomo importWpStatistics</code>', '<code>wp matomo importWpStatistics</code>' ); ?>
 
 	<h2>3. <?php esc_html_e( 'Done', 'matomo' ); ?></h2>
 	<p>
 		<?php esc_html_e( 'The data will now show up in your Matomo Summary page and Matomo Reporting. Please note that the "Visits Log" feature won\'t show any data, as we only import aggregated reports.', 'matomo' ); ?>
 		<br/>
 		<br/>
-        This page will disappear when the WP Statistics plugin is deactivated.<br/>
+		This page will disappear when the WP Statistics plugin is deactivated.<br/>
 		<br/>
 	</p>
 
