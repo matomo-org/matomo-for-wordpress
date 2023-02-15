b4fe6614403bf2d179068f82e66d22ce12e8ecfd
Lance Gilmore <lance@innocraft.com>
2023-02-15 16:39:18 +1300

    tweaked function layout

--- classes/WpMatomo.php
+++ classes/WpMatomo.php
@@ -97,21 +97,7 @@ class WpMatomo {
 				$referral->register_hooks();
 			}
 
-
-
-
-            add_action('admin_notices', function () {
-                if (isset($_GET['page']) && str_starts_with( $_GET['page'], 'matomo-' )) {
-                    $systemReport = new \WpMatomo\Admin\SystemReport(self::$settings);
-                    if ($systemReport->errorsPresent()) {
-                        echo '<div class="notice notice-warning  is-dismissible">
-                          <p>' . __('There are some errors in the', 'matomo') .
-                            ' <a href="/wp-admin/admin.php?page=matomo-systemreport">' . __('Matomo Diagnostics System report', 'matomo') . '</a> ' .
-                            __('that may prevent the plugin for working normally.', 'matomo') . '</p></div>';
-                    }
-                }
-            });
-
+            add_action('admin_notices', [$this, 'check_errors']);
 
             $chart = new Chart();
 			$chart->register_hooks();
@@ -139,8 +125,20 @@ class WpMatomo {
 				'add_settings_link',
 			]
 		);
-
 	}
+
+    public function check_errors()
+    {
+        if (isset($_GET['page']) && str_starts_with( $_GET['page'], 'matomo-' )) {
+            $systemReport = new \WpMatomo\Admin\SystemReport(self::$settings);
+            if ($systemReport->errorsPresent()) {
+                echo '<div class="notice notice-warning  is-dismissible">
+                      <p>' . __('There are some errors in the', 'matomo') .
+                    ' <a href="/wp-admin/admin.php?page=matomo-systemreport">' . __('Matomo Diagnostics System report', 'matomo') . '</a> ' .
+                    __('that may prevent the plugin for working normally.', 'matomo') . '</p></div>';
+            }
+        }
+    }
 
 	private function check_compatibility() {
 		if ( ! is_admin() ) {
