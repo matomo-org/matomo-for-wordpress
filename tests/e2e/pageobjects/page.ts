0083c8675b9d554dd4166046177c04d04bbf8795
diosmosis <diosmosis@users.noreply.github.com>
2023-10-15 19:23:09 -0700

    create initial wdio project (and update chart.js as there was no package.json and npm installing removed the existing library)

---
+++ tests/e2e/pageobjects/page.ts
@@ -0,0 +1,15 @@
+import { browser } from '@wdio/globals'
+
+/**
+* main page object containing all methods, selectors and functionality
+* that is shared across all page objects
+*/
+export default class Page {
+    /**
+    * Opens a sub page of the page
+    * @param path path of the sub page (e.g. /path/to/page.html)
+    */
+    public open (path: string) {
+        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+    }
+}
---
+++ tests/e2e/test.e2e.ts
@@ -0,0 +1,15 @@
+import { expect } from '@wdio/globals'
+import LoginPage from 'e2e/pageobjects/login.page.js'
+import SecurePage from 'e2e/pageobjects/secure.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!')
+    })
+})
+
