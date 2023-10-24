4749f73bf8fef0aa7a26159972532b2665d43506
diosmosis <diosmosis@users.noreply.github.com>
2023-10-24 05:59:33 -0700

    allow overriding tag manager storage dir via const in case matomo plugin is a symlink

--- scripts/local-dev-entrypoint.sh
+++ scripts/local-dev-entrypoint.sh
@@ -67,6 +67,7 @@ define('FORCE_SSL', false);
 define('FORCE_SSL_ADMIN', false);
 
 define( 'MATOMO_ANALYTICS_FILE', __DIR__ . '/wp-content/plugins/matomo/matomo.php' );
+define( 'MATOMO_TAG_MANAGER_STORAGE_DIR', '/../../$WORDPRESS_VERSION/wp-content/uploads/matomo/' );
 
 \$table_prefix = 'wp_';
 
