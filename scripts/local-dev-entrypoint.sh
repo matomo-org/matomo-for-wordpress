d37c44f88da3d07a2fa959caa7b6be782e3c0c52
diosmosis <diosmosis@users.noreply.github.com>
2023-10-25 11:39:57 -0700

    correct version to work on php 7.2

--- scripts/local-dev-entrypoint.sh
+++ scripts/local-dev-entrypoint.sh
@@ -190,8 +190,8 @@ done
 if [[ ! -z "$WOOCOMMERCE" && ! -d "/var/www/html/$WORDPRESS_FOLDER/wp-content/plugins/woocommerce" ]]; then
   echo "setting up woocommerce..."
 
-  if php -r 'exit(version_compare(PHP_VERSION, "7.4", "<") ? 0 : 1);'; then
-    WOOCOMMERCE_VERSION="--version=8.1.1"
+  if php -r 'exit(version_compare(PHP_VERSION, "7.3", "<") ? 0 : 1);'; then
+    WOOCOMMERCE_VERSION="--version=7.6.1"
   fi
 
   # install woocommerce and stripe payment gateway
