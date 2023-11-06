b68de5a07b93cf8e41599ea2c40d41a1f598ed51
diosmosis <diosmosis@users.noreply.github.com>
2023-11-05 17:55:41 -0800

    when updating core, need to keep less files in vue/src folders

--- scripts/remove_not_needed_assets.sh
+++ scripts/remove_not_needed_assets.sh
@@ -16,7 +16,7 @@ rm -rf $MATOMO_ROOT/vendor/doctrine/cache/lib/Doctrine/Common/Cache/RiakCache.ph
 find $MATOMO_ROOT/node_modules -name '*.js' ! -name 'materialize.min.js' -exec rm -rf {} +
 find $MATOMO_ROOT/node_modules -name '*.map' -exec rm -rf {} +
 find $MATOMO_ROOT/libs/jqplot -name '*.js' -exec rm -rf {} +
-find $MATOMO_ROOT/plugins/*/vue/src -name '*' -exec rm -rf {} +
+find $MATOMO_ROOT/plugins/*/vue/src -name '*.ts' -o -name '*.vue' -exec rm -rf {} +
 find $MATOMO_ROOT/plugins/*/javascripts -name '*.js' ! -name 'Piwik_Overlay.js' ! -name 'optOut.js' ! -name 'previewmode.js' ! -name 'previewmodedetection.js' ! -name 'tagmanager.js' ! -name 'tagmanager.min.js' -exec rm -rf {} +
 
 # delete some empty directories like config, angularjs, javascripts, lang, ...
