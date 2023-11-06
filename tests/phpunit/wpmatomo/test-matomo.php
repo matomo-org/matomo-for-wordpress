5941530664faeb8e1440f7dee53613b53e318752
diosmosis <diosmosis@users.noreply.github.com>
2023-11-05 19:28:57 -0800

    add unit test for matomo_rel_path

--- tests/phpunit/wpmatomo/test-matomo.php
+++ tests/phpunit/wpmatomo/test-matomo.php
@@ -10,4 +10,21 @@ class MatomoTest extends MatomoUnit_TestCase {
 		$this->assertTrue( matomo_has_compatible_content_dir() );
 	}
 
+	/**
+	 * @dataProvider get_test_data_for_matomo_rel_path
+	 */
+	public function test_matomo_rel_path( $to_dir, $from_dir, $expected_rel_path ) {
+		$actual = matomo_rel_path( $to_dir, $from_dir );
+		$this->assertEquals( $expected_rel_path, $actual );
+	}
+
+	public function get_test_data_for_matomo_rel_path() {
+		return [
+			['/var/www/html/wordpress', '/var/www/html/wordpress/matomo/path', '../../'],
+			['/var/www/html/wordpress/matomo/path', '/var/www/html/wordpress', 'matomo/path'],
+			['/var/www/html/wordpress', '/var/www/html/wordpress', ''],
+			['/var/www/html/wordpress', '/var/www/matomo/for/wordpress', '../../matomo/for/wordpress'],
+			['/var/www/matomo/for/wordpress', '/var/www/html/wordpress', '../../../html/wordpress'],
+		];
+	}
 }
