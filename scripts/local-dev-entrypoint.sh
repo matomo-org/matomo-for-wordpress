b20f56314839c2687ff7413aedf48f14495155c5
diosmosis <diosmosis@users.noreply.github.com>
2023-11-11 04:07:48 -0800

    debug e2e tests

--- scripts/local-dev-entrypoint.sh
+++ scripts/local-dev-entrypoint.sh
@@ -25,6 +25,9 @@ echo
 
 if [[ "$EXECUTE_WP_CLI" = "1" ]]; then
   /var/www/html/wp-cli.phar --path=/var/www/html/$WORDPRESS_FOLDER "$@"
+
+  echo "mwp dir:"
+  ls /var/www/html/matomo-for-wordpress/
   exit $?
 elif [[ "$EXECUTE_CONSOLE" = "1" ]]; then
   echo "<?php # /var/www/html/$WORDPRESS_FOLDER/wp-load.php" > /var/www/html/matomo.wpload_dir.php
