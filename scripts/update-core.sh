0934b5a533bec9e633e1b18e586a9b31463041c0
diosmosis <diosmosis@users.noreply.github.com>
2023-11-15 02:25:24 -0800

    HIRING.md file no longer present

--- scripts/update-core.sh
+++ scripts/update-core.sh
@@ -84,7 +84,6 @@ rm -r $MATOMO_ROOT/plugins/*/screenshots
 rm -r $MATOMO_ROOT/tmp/CACHEDIR.TAG
 find $MATOMO_ROOT/plugins/Morpheus/icons \( -type f -o -type l \) -not -path "$MATOMO_ROOT/plugins/Morpheus/icons/dist/*" -exec rm -rf {} +
 find $MATOMO_ROOT -name "*.spec.js" -exec rm -rf {} +
-rm $MATOMO_ROOT/HIRING.md
 rm $MATOMO_ROOT/.travis.yml
 rm $MATOMO_ROOT/.scrutinizer.yml
 rm $MATOMO_ROOT/.coveralls.yml
