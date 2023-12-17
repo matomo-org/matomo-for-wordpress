1dd123bca5eff2b5055270f3aa11e1f9eb02aca4
diosmosis <diosmosis@users.noreply.github.com>
2023-12-17 03:54:21 -0800

    try to fix tests

--- tests/e2e/matomo-admin.privacy.e2e.ts
+++ tests/e2e/matomo-admin.privacy.e2e.ts
@@ -24,7 +24,10 @@ describe('Matomo Admin > Privacy', () => {
 
     await $('#anonymizeStartDate').waitForExist({ timeout: 2000 });
 
-    await browser.pause(1000); // wait for controls to be initialized to today's date
+    // wait for controls to be initialized to today's date
+    await browser.waitUntil(function () {
+      return !!$('#anonymizeStartDate').val().length && !!$('#anonymizeEndDate').val().length;
+    });
 
     await browser.execute(function () {
       $('#anonymizeStartDate').val('2023-12-05');
