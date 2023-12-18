4c9e6056dd7f7812433c24cf06708b432152d132
diosmosis <diosmosis@users.noreply.github.com>
2023-12-18 12:14:47 -0800

    fix random failure hopefully

--- tests/e2e/tracking.e2e.ts
+++ tests/e2e/tracking.e2e.ts
@@ -6,7 +6,7 @@
  *
  */
 
-import { expect } from '@wdio/globals'
+import { expect, browser } from '@wdio/globals'
 import BlogHomepage from './pageobjects/blog-homepage.page.js';
 import BlogPostPage from './pageobjects/blog-post.page.js';
 import MatomoApi from './apiobjects/matomo.api.js';
@@ -30,6 +30,8 @@ describe('Tracking', () => {
 
     await BlogPostPage.open();
     await BlogPostPage.waitForTrackingRequest();
+
+    await browser.pause(500); // just to make sure everything gets tracked
 
     const counters = await MatomoApi.call('GET', 'Live.getCounters', new URLSearchParams({
       idSite: '1',
