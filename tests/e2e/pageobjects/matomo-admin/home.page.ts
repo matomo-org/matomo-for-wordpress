1cd88001098e13bfe0d68df1c8c447c45dba06ed
dizzy <diosmosis@users.noreply.github.com>
2023-12-29 21:08:06 -0800

    add e2e test for matomo admin home (#989)
    * add e2e test for matomo admin home
    
    * wait for elements
    
    * wait better?
    
    * wait better?
    
    * update expected test file
    
    * move to correct folder

---
+++ tests/e2e/pageobjects/matomo-admin/home.page.ts
@@ -0,0 +1,23 @@
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+import { $ } from '@wdio/globals';
+import MatomoAdminPage from './matomo-admin.page.js';
+
+class AdminHomePage extends MatomoAdminPage {
+  async open() {
+    const result = await super.open('CoreAdminHome.home');
+
+    await $('.theWidgetContent').waitForDisplayed();
+    await browser.pause(1000);
+
+    return result;
+  }
+}
+
+export default new AdminHomePage();
