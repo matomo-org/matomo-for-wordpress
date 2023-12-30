a30b4f9645365b5d658b53f1c82df0833ac4be25
dizzy <diosmosis@users.noreply.github.com>
2023-12-30 15:39:14 -0800

    add e2e tests for diagnostic matomo admin pages (#992)
    * add e2e tests for diagnostic matomo admin pages
    
    * add expected screenshot and better wait
    
    * add expected screenshot file
    
    * add longer timeout

---
+++ tests/e2e/matomo-admin.diagnostic.e2e.ts
@@ -0,0 +1,34 @@
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+import { expect, browser } from '@wdio/globals';
+import TrackingFailuresPage from './pageobjects/matomo-admin/diagnostic/tracking-failures.page.js';
+import DeviceDetectionPage from './pageobjects/matomo-admin/diagnostic/device-detection.page.js';
+import Website from './website.js';
+
+describe('Matomo Admin > Diagnostic', () => {
+  before(async () => {
+    await Website.login();
+  });
+
+  it('should load the tracking failures page correctly', async () => {
+    await TrackingFailuresPage.open();
+
+    await expect(
+      await browser.checkFullPageScreen('matomo-admin.diagnostic.tracking-failures')
+    ).toEqual(0);
+  });
+
+  it('should load the device detection page correctly', async () => {
+    await DeviceDetectionPage.open();
+
+    await expect(
+      await browser.checkFullPageScreen('matomo-admin.diagnostic.device-detection')
+    ).toEqual(0);
+  });
+});
