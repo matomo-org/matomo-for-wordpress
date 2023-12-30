c24e7aaff12701fd79cf30ed9a08d671f49f2738
diosmosis <diosmosis@users.noreply.github.com>
2023-12-30 14:18:38 -0800

    add e2e tests for matomo admin system pages

---
+++ tests/e2e/pageobjects/matomo-admin/system/general-settings.page.ts
@@ -0,0 +1,20 @@
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+import { $ } from '@wdio/globals';
+import MatomoAdminPage from '../matomo-admin.page.js';
+
+class GeneralSettingsPage extends MatomoAdminPage {
+  async open() {
+    const result = await super.open('CoreAdminHome.generalSettings');
+
+    await $('#CoreAdminHomePluginSettings .form-group').waitForDisplayed();
+
+    return result;
+  }
+}
---
+++ tests/e2e/pageobjects/matomo-admin/system/mobile-messaging.page.ts
@@ -0,0 +1,20 @@
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+import { $ } from '@wdio/globals';
+import MatomoAdminPage from '../matomo-admin.page.js';
+
+class MobileMessagingPage extends MatomoAdminPage {
+  async open() {
+    const result = await super.open('MobileMessaging.index');
+
+    await $('.manageMobileMessagingSettings').waitForDisplayed();
+
+    return result;
+  }
+}
