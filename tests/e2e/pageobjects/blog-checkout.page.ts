ec22a02c6f2684b42a23b18953f9c838be632a39
diosmosis <diosmosis@users.noreply.github.com>
2024-01-12 06:51:43 -0800

    input fields are also different in CI

--- tests/e2e/pageobjects/blog-checkout.page.ts
+++ tests/e2e/pageobjects/blog-checkout.page.ts
@@ -11,19 +11,19 @@ import Page from './page.js';
 class BlogCheckoutPage extends Page {
   async order() {
     console.log(await browser.execute(() => document.body.innerHTML));
-    await $('input#email').setValue('testemail@example.com');
-    await $('input#billing-first_name').setValue('FirstName');
-    await $('input#billing-last_name').setValue('McLastNamington');
-    await $('input#billing-address_1').setValue('200 Santa Monica Pier');
-    await $('input#billing-city').setValue('Santa Monica');
+    await $('input#email,#billing_email_field').setValue('testemail@example.com');
+    await $('input#billing-first_name,#billing_first_name').setValue('FirstName');
+    await $('input#billing-last_name,#billing_last_name').setValue('McLastNamington');
+    await $('input#billing-address_1,#billing_address_1').setValue('200 Santa Monica Pier');
+    await $('input#billing-city,#billing_city').setValue('Santa Monica');
     await browser.execute(() => {
-      window.jQuery('#billing-state input').val('California');
+      window.jQuery('#billing-state input,#billing_state').val('California');
     });
-    await $('input#billing-postcode').setValue('90401');
+    await $('input#billing-postcode,#billing_postcode').setValue('90401');
     await browser.pause(1500);
 
     await browser.execute(() => {
-      window.jQuery('.wc-block-components-checkout-place-order-button')[0].click();
+      window.jQuery('.wc-block-components-checkout-place-order-button,#place_order')[0].click();
     });
 
     await $('li.woocommerce-order-overview__order').waitForDisplayed();
