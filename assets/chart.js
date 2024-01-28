4e497fc2a7b715ece9a3c26f245b07c63b2ab703
diosmosis <diosmosis@users.noreply.github.com>
2024-01-28 00:34:30 -0800

    remove unneeded change

--- assets/chart.js
+++ assets/chart.js
@@ -44,7 +44,7 @@ jQuery(document).ready(function(){
                 scales: {
                     y: {
                         beginAtZero: true
-                    },
+                    }
                 }
             }
         });
