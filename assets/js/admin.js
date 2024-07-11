758fe4198852632ebfc2d743dd68c25c8be1e6f6
diosmosis <diosmosis@users.noreply.github.com>
2024-07-10 21:27:32 -0700

    use nonces for notice dismiss ajax requests and put scheduled task notice dismissal into admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -6,17 +6,43 @@
  * @package matomo
  */
 
-jQuery(document).on( 'click', '#matomo-referral .notice-dismiss', function () {
-    var data = {'action': 'matomo_referral_dismiss_admin_notice'};
-    jQuery.post( ajaxurl, data );
-});
+window.jQuery(document).ready(function ($) {
+  // referral notice dismiss
+  if (typeof mtmReferralDismissNoticeAjax !== 'undefined' && mtmReferralDismissNoticeAjax.ajax_url) {
+    $(document).on( 'click', '#matomo-referral .notice-dismiss', function () {
+      $.post(mtmReferralDismissNoticeAjax.ajax_url, {
+        _ajax_nonce: mtmReferralDismissNoticeAjax.nonce,
+        action: 'matomo_referral_dismiss_admin_notice',
+      });
+    });
 
-jQuery(document).on( 'click', '#matomo-referral .matomo-dismiss-forever', function () {
-    var data = {'action': 'matomo_referral_dismiss_admin_notice', forever: '1'};
-    jQuery.post( ajaxurl, data );
-});
+    $(document).on( 'click', '#matomo-referral .matomo-dismiss-forever', function () {
+      $.post(mtmReferralDismissNoticeAjax.ajax_url, {
+        _ajax_nonce: mtmReferralDismissNoticeAjax.nonce,
+        action: 'matomo_referral_dismiss_admin_notice',
+        forever: '1',
+      });
+    });
+  }
 
-jQuery(document).on( 'click', '#matomo-systemreporterrors .notice-dismiss', function () {
-    var data = {'action': 'matomo_system_report_error_dismissed'};
-    jQuery.post( ajaxurl, data );
+  // system report error dismiss
+  if (typeof mtmSystemReportErrorNoticeAjax !== 'undefined' && mtmSystemReportErrorNoticeAjax.ajax_url) {
+    $(document).on( 'click', '#matomo-systemreporterrors .notice-dismiss', function () {
+      $.post(mtmSystemReportErrorNoticeAjax.ajax_url, {
+        _ajax_nonce: mtmSystemReportErrorNoticeAjax.nonce,
+        action: 'matomo_system_report_error_dismissed',
+      });
+    });
+  }
+
+  // scheduled task error dismiss
+  if (typeof mtmScheduledTaskErrorAjax !== 'undefined' && mtmScheduledTaskErrorAjax.ajax_url) {
+    $('body').on('click', '.matomo-cron-error .notice-dismiss', function (e) {
+      $.post(mtmScheduledTaskErrorAjax.ajax_url, {
+        _ajax_nonce: mtmScheduledTaskErrorAjax.nonce,
+        action: 'mtm_remove_cron_error',
+        matomo_job_id: $(e.target).closest('.matomo-cron-error').data('job')
+      });
+    });
+  }
 });
