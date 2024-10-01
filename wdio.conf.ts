59a42130b998e180d3bd407c17841bc0093b3cca
diosmosis <diosmosis@users.noreply.github.com>
2024-10-01 04:14:27 -0700

    concat temporary debug.log files from tests into one big one that is output in github action step

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -26,15 +26,19 @@ async function saveScreenshotIfError(test, error) {
 
 function checkWpDebugLogsForError() {
   const wpDebugLogPath = path.join(dirname, 'docker', 'wordpress', WORDPRESS_DIR_NAME, 'wp-content', 'debug.log');
+  const wpDebugLogConcatPath = path.join(dirname, 'docker', 'wordpress', WORDPRESS_DIR_NAME, 'wp-content', 'debug.concat.log');
 
   if (!fs.existsSync(wpDebugLogPath)) {
     return;
   }
 
   try {
-    let contents = fs.readFileSync(wpDebugLogPath).toString('utf-8').split("\n");
+    let contents = fs.readFileSync(wpDebugLogPath).toString('utf-8');
 
-    let matomoErrors = contents.filter((line) => {
+    fs.appendFileSync(wpDebugLogConcatPath, contents);
+
+    let lines = contents.split("\n");
+    let matomoErrors = lines.filter((line) => {
       return /(notice|warning|error|deprecated):/i.test(line) && line.toLowerCase().includes('matomo');
     });
 
