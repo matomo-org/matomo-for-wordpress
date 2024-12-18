ac740a228e950e9145f8cda4a2ed57ff58751de1
diosmosis <diosmosis@users.noreply.github.com>
2024-12-17 20:13:40 -0800

    woocommerce 9.5.1 moves the "skip guided tour" link in the setup

--- tests/e2e/website.ts
+++ tests/e2e/website.ts
@@ -11,6 +11,8 @@ import fetch from 'node-fetch';
 import * as path from 'path';
 import * as fs from 'fs';
 
+const SKIP_SETUP_LINK_SELECTOR = '.woocommerce-profiler-navigation-skip-link,.woocommerce-profile-wizard__footer-link,.woocommerce-profiler-setup-store__button.is-tertiary';
+
 let latestWordpressVersion: string|undefined;
 
 async function getLatestWordpressVersion() {
@@ -92,8 +94,7 @@ class Website {
     const baseUrl = await this.baseUrl();
 
     await browser.url(`${baseUrl}/wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard`);
-console.log('setting up woocommerce');
-    const skipSetupLink = $('.woocommerce-profiler-navigation-skip-link,.woocommerce-profile-wizard__footer-link');
+    const skipSetupLink = $(SKIP_SETUP_LINK_SELECTOR);
     try {
       await skipSetupLink.waitForDisplayed();
     } catch (e) {
@@ -103,22 +104,15 @@ console.log('setting up woocommerce');
     const alreadyConfigured = !(await skipSetupLink.isExisting());
     if (alreadyConfigured) {
       console.log('cannot find skip setup link');
-      throw new Error('whooooops');
       return;
     }
 
     // get through guided config
-    await browser.execute(() => {
-      window.jQuery('.woocommerce-profiler-navigation-skip-link,.woocommerce-profile-wizard__footer-link')[0].click();
-    })
+    await browser.execute((s) => { window.jQuery(s)[0].click(); }, SKIP_SETUP_LINK_SELECTOR);
     await browser.pause(500);
-
-    let isWooCommerce7 = false;
 
     const possibleModalButton = $('.woocommerce-usage-modal__actions .is-secondary');
     if (await possibleModalButton.isExisting()) { // woocommerce version that works with php 7.2
-      isWooCommerce7 = true;
-
       await possibleModalButton.click();
     } else { // latest woocommerce
       await $('#woocommerce-select-control-0__help').click();
