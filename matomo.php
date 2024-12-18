13b2645bf10b5f76e1a2dc4452ca017c3d42b366
diosmosis <diosmosis@users.noreply.github.com>
2024-12-18 14:47:31 -0800

    update expected screenshot

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 5.1.7
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 9.4.3
+ * WC tested up to: 9.5.1
  *
  * Matomo - free/libre analytics platform
  *
