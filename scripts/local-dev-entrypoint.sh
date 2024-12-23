4ca4908e55d45300785318c234f77612b5f265c9
diosmosis <diosmosis@users.noreply.github.com>
2024-12-23 00:08:48 -0800

    fix failing command

--- scripts/local-dev-entrypoint.sh
+++ scripts/local-dev-entrypoint.sh
@@ -503,7 +503,7 @@ EOF
   # TODO: file permissions becoming a pain, shouldn't have to deal with this for dev env. this works for now though.
   touch /var/www/html/$WORDPRESS_FOLDER/debug.log /var/www/html/matomo.wpload_dir.php
   mkdir -p /var/www/html/$WORDPRESS_FOLDER/wp-content/uploads/matomo/tmp/cache/tracker /var/www/html/$WORDPRESS_FOLDER/wp-content/plugins/matomo/app/tmp
-  chown -R "${FIlE_OWNER_USERID:-1000}:${GID:-1000}" /var/www/html/$WORDPRESS_FOLDER/wp-content/uploads /var/www/html/$WORDPRESS_FOLDER/wp-content/cache
+  chown -R "${FIlE_OWNER_USERID:-1000}:${GID:-1000}" /var/www/html/$WORDPRESS_FOLDER/wp-content/uploads
   find "/var/www/html/$WORDPRESS_FOLDER" -path "/var/www/html/$WORDPRESS_FOLDER/wp-content/plugins/matomo" -prune -o -exec chown "${FIlE_OWNER_USERID:-1000}:${GID:-1000}" {} +
   find "/var/www/html/$WORDPRESS_FOLDER" -path "/var/www/html/$WORDPRESS_FOLDER/wp-content/plugins/matomo" -prune -o -exec chmod 0777 {} +
   chmod -R 0777 "/var/www/html/$WORDPRESS_FOLDER/wp-content/plugins/matomo/app/tmp" "/var/www/html/index.php" "/usr/local/etc/php/conf.d" "/var/www/html/$WORDPRESS_FOLDER/debug.log" /var/www/html/matomo.wpload_dir.php
@@ -542,8 +542,6 @@ function wait_for_database() {
   done
   echo "database running."
 }
-
-echo "local-dev-entrypoint" "$@"
 
 export_global
 
