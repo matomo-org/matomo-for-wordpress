567f54dcfa1d0f4b585a66fb46bedcee66107f18
diosmosis <diosmosis@users.noreply.github.com>
2025-01-17 11:00:20 -0800

    if ecommerce event is output after wp_footer is invoked, track in background

--- classes/WpMatomo/Ecommerce/Base.php
+++ classes/WpMatomo/Ecommerce/Base.php
@@ -84,7 +84,8 @@ class Base {
 	protected function should_track_background() {
 		return ( defined( 'DOING_AJAX' ) && DOING_AJAX )
 			   || ( defined( 'REST_REQUEST' ) && REST_REQUEST )
-			   || $this->settings->get_global_option( 'track_mode' ) === TrackingSettings::TRACK_MODE_TAGMANAGER;
+			   || $this->settings->get_global_option( 'track_mode' ) === TrackingSettings::TRACK_MODE_TAGMANAGER
+			   || did_action( 'wp_footer' );
 	}
 
 	protected function make_matomo_js_tracker_call( $params ) {
