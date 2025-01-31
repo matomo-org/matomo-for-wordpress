aa80acd35715960c3d184021e2a4104e9768907b
diosmosis <diosmosis@users.noreply.github.com>
2025-01-31 12:22:30 -0800

    increase timeout

--- tests/e2e/update.e2e.ts
+++ tests/e2e/update.e2e.ts
@@ -92,7 +92,7 @@ describe('MWP Updating', () => {
 
     await browser.waitUntil(async () => {
       return await browser.execute(() => window.jQuery('.matomo-whats-new').length) === 0;
-    });
+    }, { timeout: 30000 });
 
     await browser.pause(1000); // additional wait for ajax methods to complete
   });
