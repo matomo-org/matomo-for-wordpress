16b47ad81167328726c4d074818d82cd80225faf
diosmosis <diosmosis@users.noreply.github.com>
2025-02-05 03:38:17 -0800

    remove site selector with javascript?

--- plugins/WordPress/WordPress.php
+++ plugins/WordPress/WordPress.php
@@ -481,7 +481,6 @@ class WordPress extends Plugin
         $files[] = "../plugins/WordPress/stylesheets/optout.css";
         $files[] = "../plugins/WordPress/stylesheets/export.css";
         $files[] = "../plugins/WordPress/stylesheets/blogselection.css";
-        $files[] = "../plugins/WordPress/stylesheets/siteselector.css";
         $files[] = "../plugins/WordPress/vue/src/PluginMeasurableSettings/PluginMeasurableSettings.less";
     }
 }
--- plugins/WordPress/vue/src/index.ts
+++ plugins/WordPress/vue/src/index.ts
@@ -5,5 +5,11 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
+import { Matomo } from 'CoreHome';
+
 export { default as UseOptOutShortCode } from './UseOptOutShortCode/UseOptOutShortCode.vue';
 export { default as PluginMeasurableSettings } from './PluginMeasurableSettings/PluginMeasurableSettings.vue';
+
+Matomo.on('Matomo.topControlsRendered', () => {
+  $('.top_controls .siteSelector').hide();
+});
