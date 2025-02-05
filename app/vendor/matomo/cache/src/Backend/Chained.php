a9c0f63c593243e85f2c5e07ebab0628430554b7
diosmosis <diosmosis@users.noreply.github.com>
2025-02-05 13:47:44 -0800

    debug test failure

--- app/vendor/matomo/cache/src/Backend/Chained.php
+++ app/vendor/matomo/cache/src/Backend/Chained.php
@@ -78,7 +78,11 @@ class Chained implements Backend
     {
         $flushed = \true;
         foreach ($this->backends as $backend) {
+			try {
             $flushed = $backend->doFlush() && $flushed;
+			} catch (\Exception $ex) {
+				throw new Exception($ex->getMessage()); // TODO: remove
+			}
         }
         return $flushed;
     }
