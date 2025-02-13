c4e92cb464d77372ad9c69474113baf8e280f69b
diosmosis <diosmosis@users.noreply.github.com>
2025-02-13 14:26:42 -0800

    sanity check related to random e2e test failure

--- tests/e2e/pageobjects/page.ts
+++ tests/e2e/pageobjects/page.ts
@@ -137,6 +137,10 @@ export default class Page {
 
   async prepareWpAdminForScreenshot() {
     await browser.execute(() => {
+      if (!window.jQuery('#wpadminbar,#adminmenumain').length) {
+        throw new Error('cannot find elements to hide');
+      }
+
       window.jQuery('.notice-ocean-extra-plugin').hide();
       window.jQuery('.notice-ocean-extra-plugin .notice-dismiss').click();
       window.jQuery('#wpadminbar,#adminmenumain').hide();
