82291e44c3d9bbba2da25e249d253e30878343b5
diosmosis <diosmosis@users.noreply.github.com>
2025-02-26 11:28:04 -0800

    remove debugging code

--- classes/WpMatomo/Db/WordPress.php
+++ classes/WpMatomo/Db/WordPress.php
@@ -257,18 +257,9 @@ class WordPress extends Mysqli {
 			}
 		}
 
-		$before_question_sql = $sql;
 		$sql = $this->replace_placeholders( $sql );
 
-		try {
 		$query = $wpdb->prepare( $sql, $bind );
-		} catch (\Exception $ex) {
-			error_log('wpdb error: ' . $ex->getMessage() . "\n" . $ex->getTraceAsString());
-			error_log("sql was: $sql");
-			error_log("unmodified sql: $before_question_sql");
-			error_log("bind was: " . var_export($bind, true));
-			throw $ex;
-		}
 
 		if ($has_replaced_null) {
 			$query = str_replace("'$null_placeholder'", 'NULL', $query);
