1cf85e3895803efb0f5d8b119448ec8ec08a1b61
diosmosis <diosmosis@users.noreply.github.com>
2025-03-17 18:05:10 -0700

    fix property name

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -274,7 +274,7 @@ export const config: Options.Testrunner = {
       await saveScreenshotIfError(test, error);
 
       // TODO: this does not appear to display in the reporter, even though it works in the core UI screenshot tests
-      test.err.message = `${test.err.message}\nCaptured Console Logs:\n${capturedLogs.join('\n')}`;
+      test.error.message = `${test.error.message}\nCaptured Console Logs:\n${capturedLogs.join('\n')}`;
 
       return;
     }
