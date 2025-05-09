f559a67da481cc584241404bf8b8bf55b2db9f07
diosmosis <diosmosis@users.noreply.github.com>
2025-05-09 11:12:08 -0700

    save first visitor ID in WC session, if it exists, in case a users visitor ID changes their browser

--- classes/WpMatomo/AjaxTracker.php
+++ classes/WpMatomo/AjaxTracker.php
@@ -57,25 +57,35 @@ class AjaxTracker extends \MatomoTracker {
 			$this->disableCookieSupport();
 		}
 
-		if ( $this->loadVisitorIdCookie() ) {
-			if ( ! empty( $this->cookieVisitorId ) ) {
-				$this->has_cookie = true;
+		$has_wc_session = function_exists( 'WC' ) && isset( WC()->session );
+		if ( $has_wc_session ) {
+			$visitor_id = WC()->session->get( ServerSideVisitorId::VISITOR_ID_SESSION_VAR_NAME );
+			if ( ! empty( $visitor_id ) ) {
+				$this->hasCookie = true; // do not set cookies for this visitor
 				try {
-					$this->setVisitorId( $this->cookieVisitorId );
-				} catch (\Exception $ex) {
+					$this->setVisitorId( $visitor_id );
+				} catch ( \Exception $ex ) {
 					// do not fatal if the visitor ID is invalid for some reason
 					if ( ! $this->is_invalid_visitor_id_error( $ex ) ) {
 						throw $ex;
 					}
 				}
 			}
-		} else if ( function_exists( 'WC' ) && isset( WC()->session ) ) {
-			$visitor_id = WC()->session->get( ServerSideVisitorId::VISITOR_ID_SESSION_VAR_NAME );
-			if ( ! empty( $visitor_id ) ) {
-				$this->hasCookie = true; // do not set cookies for this visitor, since it would have no effect anyway
+		}
+
+		if ( empty( $this->forcedVisitorId ) && $this->loadVisitorIdCookie() ) {
+			if ( ! empty( $this->cookieVisitorId ) ) {
+				$this->has_cookie = true;
 				try {
-					$this->setVisitorId( $visitor_id );
-				} catch ( \Exception $ex ) {
+					$this->setVisitorId( $this->cookieVisitorId );
+
+					// save first used visitor ID for this session so we can keep using it, in case
+					// the visitor ID cookie in a user's browser changes (either due to developer error
+					// or some strange configuration we don't know about)
+					if ( $has_wc_session ) {
+						WC()->session->set( ServerSideVisitorId::VISITOR_ID_SESSION_VAR_NAME, $this->cookieVisitorId );
+					}
+				} catch (\Exception $ex) {
 					// do not fatal if the visitor ID is invalid for some reason
 					if ( ! $this->is_invalid_visitor_id_error( $ex ) ) {
 						throw $ex;
