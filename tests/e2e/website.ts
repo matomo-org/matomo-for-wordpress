fea42565ad0db69a9c4278349021747fec4e41eb
diosmosis <diosmosis@users.noreply.github.com>
2025-06-16 06:07:58 -0700

    add test users to local dev environment use in new e2e test

--- tests/e2e/website.ts
+++ tests/e2e/website.ts
@@ -68,7 +68,7 @@ class Website {
     this.site = siteSlug;
   }
 
-  async login() {
+  async login(user?: string, pass?: string) {
     if (this.loggedIn) {
       return;
     }
@@ -84,8 +84,8 @@ class Website {
           window.jQuery('#user_login').val(l);
           window.jQuery('#user_pass').val(p);
         },
-        process.env.WORDPRESS_USER_LOGIN || 'root',
-        process.env.WORDPRESS_USER_PASS || 'pass'
+        user || process.env.WORDPRESS_USER_LOGIN || 'root',
+        pass || process.env.WORDPRESS_USER_PASS || 'pass'
       );
       await $('#wp-submit').click();
 
@@ -95,6 +95,17 @@ class Website {
         }));
       }, { timeout: 60000 });
     });
+  }
+
+  async logout() {
+    const logoutLink = $('#wp-admin-bar-logout a');
+    if (await logoutLink.isExisting()) {
+        await browser.execute(() => {
+            window.jQuery('#wp-admin-bar-logout a')[0].click();
+        });
+
+        await $('#user_login').waitForExist({ timeout: 60000 });
+    }
   }
 
   async getWpNonce() {
