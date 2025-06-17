fd4de3430ff6839babe0905d4e7224f160ac6994
dizzy <diosmosis@users.noreply.github.com>
2025-06-16 23:48:02 -0700

    make sure Matomo roles can view the admin dashboard (#1301)
    * make sure Matomo roles can view the admin dashboard
    
    * add unit tests
    
    * add test users to local dev environment use in new e2e test
    
    * do not overwrite wpdb for wpmatomo/db/test-wordpress.php
    
    * do not overwrite wpdb for tests/phpunit/wpmatomo/test-updater.php
    
    * trim menu items in new e2e test before comparing
    
    * prevent test from throwing WPDieException when in multisite
    
    * when a view user logs in, they are redirected to their profile, not the WP dashboard
    
    * tweak comment text

--- scripts/local-dev-entrypoint.sh
+++ scripts/local-dev-entrypoint.sh
@@ -549,6 +549,23 @@ EOF
   wp_new_post post "10-new-ways-to-whatever" "Learn 10 exciting new ways to WHATEVER!"
   wp_new_post post "why-use-our-stuff" "Why you should be using our stuff and whatnot!"
 
+  # create users with matomo roles
+  echo "adding test users..."
+  function wp_new_user() {
+    ROLE="${1}_role"
+    USERID="${1//_}user"
+    USEREMAIL="${USERID}@nowhere.com"
+
+    if ! /var/www/html/wp-cli.phar --path=/var/www/html/$WORDPRESS_FOLDER --allow-root --user=$WP_ADMIN_USER user exists "$USERID"; then
+      /var/www/html/wp-cli.phar --path=/var/www/html/$WORDPRESS_FOLDER --allow-root --user=$WP_ADMIN_USER user create "$USERID" "$USEREMAIL" --user_pass="$USERID" --role="$ROLE"
+    fi
+  }
+
+  wp_new_user matomo_view
+  wp_new_user matomo_write
+  wp_new_user matomo_admin
+  wp_new_user matomo_superuser
+
   # setup everything required for unit tests
   if [ "$WORDPRESS_VERSION" = "trunk" ]; then
     WORDPRESS_SVN_FOLDER="trunk"
