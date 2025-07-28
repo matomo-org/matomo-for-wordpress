e2f85fb7191383b72fd4bb91ea0deac21870ea51
diosmosis <diosmosis@users.noreply.github.com>
2025-07-27 22:35:23 -0700

    woocommerce_item_set_quantity is called after totals are calculated, so use woocommerce_after_cart_item_quantity_update instead

--- classes/WpMatomo/Ecommerce/Woocommerce.php
+++ classes/WpMatomo/Ecommerce/Woocommerce.php
@@ -45,7 +45,7 @@ class Woocommerce extends Base {
 		add_action( 'woocommerce_add_to_cart', [ $this, 'on_cart_updated_safe' ], 0, 0 );
 		add_action( 'woocommerce_cart_item_removed', [ $this, 'on_cart_updated_safe' ], 0, 0 );
 		add_action( 'woocommerce_cart_item_restored', [ $this, 'on_cart_updated_safe' ], 0, 0 );
-		add_action( 'woocommerce_cart_item_set_quantity', [ $this, 'on_cart_updated_safe' ], 0, 0 );
+		add_action( 'woocommerce_after_cart_item_quantity_update', [ $this, 'on_cart_updated_safe' ], 0, 0 );
 		add_action( 'woocommerce_thankyou', [ $this, 'anonymise_orderid_in_url' ], 1, 1 );
 		add_action( 'woocommerce_order_status_changed', [ $this, 'on_order_status_change' ], 10, 3 );
 		add_action( 'woocommerce_after_calculate_totals', [ $this, 'after_calculate_totals' ], 99999, 0 );
