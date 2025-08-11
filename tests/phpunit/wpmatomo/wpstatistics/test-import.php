b1111ac46fe3a3d13b4a4f661db153a2de0eefc8
diosmosis <diosmosis@users.noreply.github.com>
2025-08-10 20:32:42 -0700

    fix tests for latest wp-statistics version

--- tests/phpunit/wpmatomo/wpstatistics/test-import.php
+++ tests/phpunit/wpmatomo/wpstatistics/test-import.php
@@ -93,6 +93,8 @@ class ImportTest extends MatomoAnalytics_TestCase {
 					$run_migrations = [ \WP_Statistics\Service\Database\Managers\MigrationHandler::class, 'runMigrations' ];
 				} elseif ( method_exists( \WP_Statistics\Service\Database\Managers\MigrationHandler::class, 'runSchemaMigrations' ) ) {
 					$run_migrations = [ \WP_Statistics\Service\Database\Managers\MigrationHandler::class, 'runSchemaMigrations' ];
+				} elseif ( method_exists( \WP_Statistics\Service\Database\Migrations\Schema\SchemaManager::class, 'runSchemaMigrations' ) ) {
+					$run_migrations = [ \WP_Statistics\Service\Database\Migrations\Schema\SchemaManager::class, 'runSchemaMigrations' ];
 				} else {
 					throw new \Exception( 'do not know how to run wp-statistics migrations' );
 				}
