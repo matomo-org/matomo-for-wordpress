7110e1a824670465bd46e08930c0370b0a4e8aa3
diosmosis <diosmosis@users.noreply.github.com>
2025-08-12 17:58:02 -0700

    update minimum tested version

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 5.3.1
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 10.0.4
+ * WC tested up to: 10.1.0
  *
  * Matomo - free/libre analytics platform
  *
