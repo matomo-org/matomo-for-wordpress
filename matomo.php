90d2b05c47894c8cd370ec7ad260c9ad0863166a
diosmosis <diosmosis@users.noreply.github.com>
2025-09-07 09:36:58 -0700

    fixing failing tests

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 5.3.3
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 10.1.1
+ * WC tested up to: 10.1.2
  *
  * Matomo - free/libre analytics platform
  *
