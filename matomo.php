a40bf18c449490dd42b4beccc450ab0d53d7adf4
diosmosis <diosmosis@users.noreply.github.com>
2026-02-19 06:24:19 -0800

    bump minimum woocommerce tested against version

--- matomo.php
+++ matomo.php
@@ -7,7 +7,7 @@
  * Version: 5.6.1
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 10.5.0
+ * WC tested up to: 10.5.2
  *
  * Matomo - free/libre analytics platform
  *
