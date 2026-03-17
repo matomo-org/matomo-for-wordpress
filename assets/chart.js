6968890011d05ce46c0a415b57c118ab4adab3ab
dizzy <diosmosis@users.noreply.github.com>
2026-03-17 00:02:41 -0700

    in summary in visits over time, display visits + unique visitors (#1372)
    * allow line graphs in summary page to display more than one metric and in visits over time display visits + unique visitors
    
    * tweaks to x axis display in summary line graph
    
    * upgrade expected screenshots
    
    * update expected screenshots
    
    * update expected screenshots
    
    * update screenshot and try to fix random failure

--- assets/chart.js
+++ assets/chart.js
@@ -1,39 +1,60 @@
 jQuery(document).ready(function(){
     jQuery('.matomo-table[data-chart]').each(function() {
-       let $this = jQuery(this);
-       let $postbox = $this.parents('div.postbox');
-       let $table = $postbox.find('table');
-       $table.hide();
-       let $canvas = jQuery('<canvas/>',{'id':$this.attr('data-chart')});
-       $canvas.insertAfter($table);
-       let data = [];
-       let labels = [];
-       let title = $postbox.find('h2').text();
-       let $row;
-       let value;
-       $table.find('tr').each(function() {
-           $row = jQuery(this);
-           value = $row.find('td:nth-child(2)').text();
-           if ( '-' === value ) {
-               value = 0;
-           }
-           data.push(value);
-           labels.push($row.find('td:nth-child(1)').text());
-       });
+        var LABEL_EVERY_N_TICKS = 2;
+        var colors = [
+            "#55bae7",
+            "#ddb745",
+            "#4c2a9c",
+            "#3e7b2d"
+        ];
+        var currentColorIndex = 0;
+        var $this = jQuery(this);
+        var $postbox = $this.parents('div.postbox');
+        var $table = $postbox.find('table');
+        $table.hide();
+        var $canvas = jQuery('<canvas/>',{'id':$this.attr('data-chart')});
+        $canvas.insertAfter($table);
+        var labels = [];
+        var metrics = JSON.parse($table.attr('data-metrics'));
+        var datasets = {};
+        $table.find('tr').each(function() {
+            var $row = jQuery(this);
+
+            var labelCell = $row.find('td:nth-child(1)');
+            labels.unshift(labelCell.attr('data-label') || labelCell.text());
+
+            Object.keys(metrics).forEach((metricName, i) => {
+                var metricTitle = metrics[metricName];
+
+                if (!datasets[metricName]) {
+                    var color = colors[currentColorIndex];
+                    currentColorIndex = (currentColorIndex + 1) % colors.length;
+
+                    datasets[metricName] = {
+                        label: metricTitle,
+                        data: [],
+                        borderColor: color,
+                        pointBackgroundColor: color,
+                        pointBorderColor: color,
+                        pointHoverBackgroundColor: color,
+                        pointHoverBorderColor: color,
+                    };
+                }
+
+                var value = $row.find('td:nth-child(' + (i + 2) + ')').text();
+                if ( '-' === value ) {
+                    value = 0;
+                }
+
+                datasets[metricName].data.unshift(value);
+            });
+        });
 
         var myChart = new Chart($canvas, {
             type: 'line',
             data: {
-                labels: labels.reverse(),
-                datasets: [{
-                    label: title,
-                    data: data.reverse(),
-                    borderColor: "#55bae7",
-                    pointBackgroundColor: "#55bae7",
-                    pointBorderColor: "#55bae7",
-                    pointHoverBackgroundColor: "#55bae7",
-                    pointHoverBorderColor: "#55bae7",
-                }]
+                labels: labels,
+                datasets: Object.values(datasets)
             },
             options: {
                 plugins: {
@@ -42,6 +63,18 @@ jQuery(document).ready(function(){
                     }
                 },
                 scales: {
+                    x: {
+                        ticks: {
+                            callback: function (value, index, values) {
+                                if ( index % LABEL_EVERY_N_TICKS !== 0 ) {
+                                    return '';
+                                }
+
+                                return labels[index];
+                            },
+                            maxRotation: 0,
+                        }
+                    },
                     y: {
                         beginAtZero: true
                     }
