00cd78a941fb7aecbe6ca5c916c04df79843fb61
diosmosis <diosmosis@users.noreply.github.com>
2026-03-23 04:01:34 -0700

    more random failure fix attempts

--- tests/e2e/pageobjects/matomo-admin/personal/email-reports.page.ts
+++ tests/e2e/pageobjects/matomo-admin/personal/email-reports.page.ts
@@ -41,7 +41,9 @@ class EmailReportsPage extends MatomoAdminPage {
     await browser.execute(function () {
       $('.matomo-save-button > input')[0].click();
     });
-    await $('#entityEditContainer tr').waitForDisplayed({ timeout: 30000 });
+    await browser.waitUntil(async () => {
+      return browser.execute(() => window.jQuery('#entityEditContainer tr').is(':visible'));
+    }, { timeout: 30000 });
     await this.hideDateSelectorDate();
   }
 
--- tests/e2e/website.ts
+++ tests/e2e/website.ts
@@ -433,12 +433,12 @@ class Website {
       window.jQuery('a.upload-view-toggle')[0].click();
     });
     await browser.pause(250);
-    await $('#pluginzip').waitForClickable();
+    await $('#pluginzip').waitForClickable({ timeout: 30000 });
 
     await $('#pluginzip').setValue(pathToRelease);
     await browser.pause(250);
 
-    await $('#install-plugin-submit').waitForClickable();
+    await $('#install-plugin-submit').waitForClickable({ timeout: 30000 });
     await browser.execute(() => {
       window.jQuery('#install-plugin-submit')[0].click();
     });
