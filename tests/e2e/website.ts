1c195051b4e67d687ad91abe0a437cb7b23e3561
diosmosis <diosmosis@users.noreply.github.com>
2026-03-28 09:53:58 -0700

    another attempt and fixing a random failure

--- tests/e2e/website.ts
+++ tests/e2e/website.ts
@@ -433,7 +433,7 @@ class Website {
       window.jQuery('a.upload-view-toggle')[0].click();
     });
     await browser.pause(250);
-    await $('#pluginzip').waitForClickable();
+    await $('#pluginzip').waitForExist({ timeout: 10000 });
 
     await $('#pluginzip').setValue(pathToRelease);
     await browser.pause(250);
