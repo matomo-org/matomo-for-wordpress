e83e1b4f9575df1e967e9a138f89ab2cd3586c40
diosmosis <diosmosis@users.noreply.github.com>
2026-04-29 02:50:04 -0700

    debug unit test failure that only occurs in CI

--- tests/phpunit/wpmatomo/admin/pluginsuggestions/suggestions/test-heatmapsessionrecording.php
+++ tests/phpunit/wpmatomo/admin/pluginsuggestions/suggestions/test-heatmapsessionrecording.php
@@ -34,9 +34,6 @@ class HeatmapSessionRecordingTest extends MatomoAnalytics_TestCase {
 		$this->assertFalse( $suggestion->should_trigger() );
 	}
 
-	/**
-	 * @group only
-	 */
 	public function test_get_trigger_desc_long_includes_bounce_rate_in_text() {
 		$this->track_test_data();
 
