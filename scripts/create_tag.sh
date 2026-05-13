0c8ae8390409817cc1638ee89c85af5597a8b8d4
diosmosis <diosmosis@users.noreply.github.com>
2026-05-12 21:04:30 -0700

    remove unused scripts, update marketplace submodule and modify release script to use tag instead of live branch

--- scripts/create_tag.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-VERSION="$1"
-
-hub release create --message $VERSION --message "If you download this release, make sure the directory name within the 'wordpress/wp-content' directory is 'matomo' and not for example 'matomo-for-wordpress'. [View changes](https://github.com/matomo-org/matomo-for-wordpress/blob/develop/CHANGELOG.md)"  -t live  $VERSION
