aeb50608308b4d87cdd2d53c02dc380457fb2b75
diosmosis <diosmosis@users.noreply.github.com>
2026-05-14 04:48:19 -0700

    run promo test before plugins are installed

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -113,6 +113,7 @@ export const config: Options.Testrunner = {
   // Patterns to exclude. (all executed without parallelism before these tests, see wdio.conf.tracking.ts)
   exclude: [
     './tests/e2e/update.e2e.ts',
+    './tests/e2e/matomo-reporting.promos.e2e.ts',
     './tests/e2e/mwp-admin.marketplace.e2e.ts',
     './tests/e2e/tracking.e2e.ts',
     './tests/e2e/tracking.ecommerce.e2e.ts',
