e6b946413972e8a4c41488413cf2f14379f95adb
dizzy <diosmosis@users.noreply.github.com>
2026-07-20 15:31:06 -0700

    fix user sync collision that can occur when a user is deleted from Matomo (#1429)
    * delete wp login mappings after deleting a matomo user, and better handle the unexpected state of a dangling mapping
    
    * when deleting mappings, make sure the mappings are deleted for every blog
    
    * matomo instance is only shared between multiple blogs in network mode

--- classes/WpMatomo/User.php
+++ classes/WpMatomo/User.php
@@ -17,6 +17,11 @@ class User {
 	const USER_MAPPING_PREFIX = 'matomo-user-login-';
 
 	/**
+	 * @var Settings
+	 */
+	private $settings;
+
+	/**
 	 * @api
 	 */
 	public function get_current_matomo_user_login() {
@@ -37,7 +42,108 @@ class User {
 		}
 	}
 
+	/**
+	 * @param string $matomo_user_login
+	 * @return int[]
+	 */
+	public function get_wp_user_ids_for_matomo_login( $matomo_user_login ) {
+		global $wpdb;
+
+		if ( empty( $matomo_user_login ) ) {
+			return [];
+		}
+
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
+		$option_names = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE %s AND option_value = %s",
+				$wpdb->esc_like( self::USER_MAPPING_PREFIX ) . '%',
+				$matomo_user_login
+			)
+		);
+
+		$wp_user_ids = [];
+		foreach ( $option_names as $option_name ) {
+			$wp_user_id = (int) substr( $option_name, strlen( self::USER_MAPPING_PREFIX ) );
+			if ( $wp_user_id ) {
+				$wp_user_ids[] = $wp_user_id;
+			}
+		}
+
+		return array_unique( $wp_user_ids );
+	}
+
+	/**
+	 * @param string $matomo_user_login
+	 */
+	public function delete_mappings_for_matomo_login( $matomo_user_login ) {
+		if ( empty( $matomo_user_login ) ) {
+			return;
+		}
+
+		// on network-enabled multisite WP, there is one Matomo per network, so
+		// iterating over all blogs is not needed.
+		if ( ! $this->get_settings()->is_network_enabled() ) {
+			$this->delete_mappings_for_matomo_login_on_current_blog( $matomo_user_login );
+			return;
+		}
+
+		global $wpdb;
+
+		// the same Matomo login can be mapped from several blogs, so the mapping needs to be deleted
+		// for every blog it has been mapped within
+		$cache_key   = 'blog_ids_for_mapping_cleanup';
+		$cache_group = 'matomo';
+
+		$blogs = wp_cache_get( $cache_key, $cache_group );
+		if ( false === $blogs ) {
+			// short lived cache so a sync deleting many users does not re-run this once per user;
+			// a slightly stale blog list is harmless here (deleted blogs are skipped below).
+			// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching
+			$blogs = $wpdb->get_results( 'SELECT blog_id, deleted FROM ' . $wpdb->blogs . ' ORDER BY blog_id', ARRAY_A );
+
+			if ( is_array( $blogs ) ) {
+				wp_cache_set( $cache_key, $blogs, $cache_group, 5 * MINUTE_IN_SECONDS );
+			}
+		}
+
+		if ( ! is_array( $blogs ) ) {
+			return;
+		}
+
+		foreach ( $blogs as $blog ) {
+			if ( 1 === (int) $blog['deleted'] ) {
+				continue;
+			}
+
+			switch_to_blog( $blog['blog_id'] );
+			try {
+				$this->delete_mappings_for_matomo_login_on_current_blog( $matomo_user_login );
+			} finally {
+				restore_current_blog();
+			}
+		}
+	}
+
+	/**
+	 * @param string $matomo_user_login
+	 */
+	private function delete_mappings_for_matomo_login_on_current_blog( $matomo_user_login ) {
+		foreach ( $this->get_wp_user_ids_for_matomo_login( $matomo_user_login ) as $wp_user_id ) {
+			delete_option( self::USER_MAPPING_PREFIX . $wp_user_id );
+		}
+	}
+
 	public function uninstall() {
 		Uninstaller::uninstall_options( self::USER_MAPPING_PREFIX );
+	}
+
+	private function get_settings() {
+		if ( ! empty( $this->settings ) ) {
+			return $this->settings;
+		}
+
+		$this->settings = \WpMatomo::$settings ? \WpMatomo::$settings : new Settings();
+		return $this->settings;
 	}
 }
--- plugins/WordPress/WordPress.php
+++ plugins/WordPress/WordPress.php
@@ -70,7 +70,7 @@ class WordPress extends Plugin
             'SitesManager.shouldPerformEmptySiteCheck' => 'shouldPerformEmptySiteCheck',
             'API.TagManager.getContainerInstallInstructions.end' => 'addInstallInstructions',
             'API.Tour.getChallenges.end' => 'modifyTourChallenges',
-	        'API.ScheduledReports.generateReport.end' => 'onGenerateReportEnd',
+	          'API.ScheduledReports.generateReport.end' => 'onGenerateReportEnd',
             'API.CorePluginsAdmin.getSystemSettings.end' => 'onGetSystemSettingsEnd',
             'API.SitesManager.updateSite' => 'allowUpdateSiteForMeasurableSettings',
             'API.SitesManager.updateSite.end' => 'reDisableSitesAdmin',
@@ -84,8 +84,19 @@ class WordPress extends Plugin
             'Template.jsGlobalVariables' => 'addJsGlobalVariables',
             'API.Request.dispatch' => 'onApiRequestDispatch',
             'API.Request.dispatch.end' => 'onApiRequestDispatchEnd',
+            'UsersManager.deleteUser' => 'onDeleteMatomoUser',
             ProcessedReportInnerCallHooks::PROCESSED_REPORT_INNER_END_EVENT => 'afterProcessedReportInner',
         );
+    }
+
+    /**
+     * Delete login mappings when a Matomo user is deleted.
+     *
+     * @param string $userLogin
+     */
+    public function onDeleteMatomoUser($userLogin)
+    {
+        ( new WpMatomo\User() )->delete_mappings_for_matomo_login($userLogin);
     }
 
     public function shouldPerformEmptySiteCheck( &$shouldPerformEmptySiteCheck ) {
