37b0b3ffc22fe32f35b64505983e3ced1d95b2de
diosmosis <diosmosis@users.noreply.github.com>
2026-09-08 19:39:46 -0700

    update woocommerce tested against version

--- matomo.php
+++ matomo.php
@@ -9,7 +9,7 @@
  * Requires PHP: 7.2.5
  * Domain Path: /languages
  * WC requires at least: 2.4.0
- * WC tested up to: 11.0.1
+ * WC tested up to: 11.1.0
  *
  * Matomo - free/libre analytics platform
  *
